653430ba339134771f8b1097cb4b57d0b15772b7
tdayris <tdayris@gustaveroussy.fr>
2020-04-16 16:57:08 +0200

    [dev] (enrichNCG): Initial commit

---
+++ bio/clusterProfiler/enrichNCG/wrapper.R
@@ -0,0 +1,69 @@
+#!/usr/bin/R
+
+# This script takes a geneList object and performs
+# an enrichment analysis based on DOSE database
+
+# __author__ = "Thibault Dayris"
+# __copyright__ = "Copyright 2020, Thibault Dayris"
+# __email__ = "thibault.dayris@gustaveroussy.fr"
+# __license__ = "MIT"
+
+# Perform gene enrichment
+base::library(package = "clusterProfiler", quietly = TRUE);
+# Loading databases
+base::library(package = "org.Hs.eg.db", quietly = TRUE);
+base::library(package = "org.Mm.eg.db", quietly = TRUE);
+
+
+# Loading input dataset
+geneList <- base::readRDS(
+  file = snakemake@input[["rds"]]
+);
+
+organism <- org.Hs.eg.db;
+if ("organism" %in% base::names(snakemake@params)) {
+  if (snakemake@params[["organism"]] == "Mm") {
+    organism <- org.Mm.eg.db;
+  }
+}
+
+extra <- "gene = geneList, readable = FALSE, universe = names(geneList)";
+if ("enrichNCG_extra" %in% snakemake@params) {
+  extra <- base::paste(
+    extra,
+    snakemake@params[["enrichNCG_extra"]],
+    sep = ", "
+  )
+}
+
+command <- base::paste0(
+  "clusterProfiler::enrichNCG(",
+  extra,
+  ")"
+);
+base::message("Libraries and datasets loaded");
+base::message(command);
+
+# Performing DOSE enrichment
+encg <- base::eval(
+  base::parse(
+    text = command
+  )
+);
+
+encg <- clusterProfiler::setReadable(
+    encg,
+    organism,
+    keytype = "ENTREZ"
+);
+
+# Saving results
+base::saveRDS(
+  obj = encg,
+  file = snakemake@output[["rds"]]
+);
+
+utils::write.table(
+  x = encg,
+  file = snakemake@output[["tsv"]]
+);
