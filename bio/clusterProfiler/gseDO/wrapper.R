a91bab25738506af6ff4a572b2fd221b7d2e4523
tdayris <tdayris@gustaveroussy.fr>
2020-04-17 12:32:51 +0200

    [dev] (gseDO): Initial commit

---
+++ bio/clusterProfiler/gseDO/wrapper.R
@@ -0,0 +1,61 @@
+#!/usr/bin/R
+
+# This script takes a geneList object and performs
+# an enrichment analysis based on DOSE database
+
+# __author__ = "Thibault Dayris"
+# __copyright__ = "Copyright 2020, Thibault Dayris"
+# __email__ = "thibault.dayris@gustaveroussy.fr"
+# __license__ = "MIT"
+
+# Perform gene enrichment
+base::library(package = "DOSE", quietly = TRUE);
+base::library(package = "clusterProfiler", quietly = TRUE);
+# Loading databases
+base::library(package = "org.Hs.eg.db", quietly = TRUE);
+
+
+# Loading input dataset
+geneList <- base::readRDS(
+  file = snakemake@input[["rds"]]
+);
+
+extra <- "geneList, verbose = TRUE";
+if ("gseDO_extra" %in% snakemake@params) {
+  extra <- base::paste(
+    extra,
+    snakemake@params[["gseDO_extra"]],
+    sep = ", "
+  )
+}
+
+command <- base::paste0(
+  "DOSE::gseDO(",
+  extra,
+  ")"
+);
+base::message("Libraries and datasets loaded");
+base::message(command);
+
+# Performing DOSE enrichment
+gse_do <- base::eval(
+  base::parse(
+    text = command
+  )
+);
+
+gse_do <- DOSE::setReadable(
+    x = gse_do,
+    OrgDb = org.Hs.eg.db
+);
+
+# Saving results
+base::saveRDS(
+  obj = gse_do,
+  file = snakemake@output[["rds"]]
+);
+
+utils::write.table(
+  x = gse_do,
+  file = snakemake@output[["tsv"]]
+);
