86850364cf8bf9dfab5d0a6213c773dbbd03dd58
tdayris <tdayris@gustaveroussy.fr>
2020-04-21 10:38:41 +0200

    [dev] (dotplot): New tested wrapper

---
+++ bio/clusterProfiler/dotplot/wrapper.R
@@ -0,0 +1,56 @@
+#!/usr/bin/R
+
+# This script takes an enriched terms object from clusterProfiler
+# and builds a dotplot of most enriched terms or provided list of
+# pathways
+
+# __author__ = "Thibault Dayris"
+# __copyright__ = "Copyright 2020, Thibault Dayris"
+# __email__ = "thibault.dayris@gustaveroussy.fr"
+# __license__ = "MIT"
+
+# Handle enrichment
+base::library(package = "clusterProfiler", quietly = TRUE);
+# Handle graphics
+base::library(package = "Cairo", quietly = TRUE);
+base::library(package = "enrichplot", quietly = TRUE);
+
+enriched <- base::readRDS(
+  file = base::as.character(x = snakemake@input[["rds"]])
+);
+
+dotplot_extra <- "object = enriched";
+if ("dotplot_extra" %in% base::names(snakemake@params)) {
+  dotplot_extra <- base::paste(
+    dotplot_extra,
+    snakemake@params[["dotplot_extra"]],
+    sep = ", "
+  );
+}
+base::message("Libraries and input data loaded");
+
+# Build command line
+command <- base::paste0(
+  "enrichplot::dotplot(",
+  dotplot_extra,
+  ")"
+);
+base::message(command);
+
+# Build plot
+png(
+  filename = snakemake@output[["png"]],
+  width = 1024,
+  height = 768,
+  units = "px",
+  type = "cairo"
+);
+
+
+base::eval(
+  base::parse(
+    text = command
+  )
+);
+
+dev.off()
