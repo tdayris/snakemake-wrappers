c2c7082d47ce2056ca8cc528d4f1fce15691b2e8
tdayris <tdayris@gustaveroussy.fr>
2020-04-24 11:21:19 +0200

    [dev] (preFiler): Initial commit

---
+++ bio/isoformSwitchAnalyseR/preFilter/wrapper.R
@@ -0,0 +1,49 @@
+#!/usr/bin/R
+
+# This script takes a IsoformSwitchAnalyseR object
+# and filters it
+
+# __author__ = "Thibault Dayris"
+# __copyright__ = "Copyright 2020, Thibault Dayris"
+# __email__ = "thibault.dayris@gustaveroussy.fr"
+# __license__ = "MIT"
+
+
+# The main package
+base::library(package = "isoformSwitchAnalyzeR", quietly = TRUE);
+
+# Gathering input dataset and parameters
+switch_list <- base::as.character(
+  x = snakemake@input[["switch_list"]]
+);
+
+extra <- "switchAnalyzeRlist = switch_list";
+if ("extra" %in% base::names(snakemake@params)) {
+  extra <- base::paste(
+    extra,
+    snakemake@params[["extra"]],
+    sep = ", "
+  );
+}
+
+# Buiding command line itself
+command <- base::paste0(
+  "InsoformSwitchAnalyseR::preFilter(",
+  extra,
+  ")"
+);
+base::message("Libraries and input data loaded");
+base::message(command);
+
+# Running command
+base::eval(
+  base::parse(
+    text = command
+  )
+);
+
+# Saving results
+base::saveRDS(
+  obj = edgn,
+  file = snakemake@output[["rds"]]
+);
