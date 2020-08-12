d08f0d123bfa1b4d822217c7033f11cee81bdfab
tdayris <tdayris@gustaveroussy.fr>
2020-08-12 13:47:40 +0200

    [dev] (iRODS/yaml): Now takes duplicated keys

--- bio/iRODS/yaml/wrapper.py
+++ bio/iRODS/yaml/wrapper.py
@@ -19,12 +19,14 @@ attributes = {}
 
 current_attribute = None
 
+
 def get_key_val(line: str) -> Dict[str, str]:
     """
     Split key:value string
     """
     key, *val = line.split(":")
     return {key.strip(" "): ":".join(val).strip(" ")}
+
 
 with open(snakemake.input[0], "r") as text:
     for line in text:
@@ -35,15 +37,19 @@ with open(snakemake.input[0], "r") as text:
         elif line.startswith("attribute:"):
             current_attribute = get_key_val(line=line[:-1])["attribute"]
             if file_path in attributes.keys():
-                attributes[file_path][current_attribute] = {}
+                if current_attribute not in attributes[file_path].keys():
+                    attributes[file_path][current_attribute] = {}
             else:
                 attributes[file_path] = {current_attribute: {}}
         elif line.startswith(("value:", "units:")):
-            parsed = get_key_val(line[:-1])
-            try:
-                attributes[file_path][current_attribute].update(parsed)
-            except KeyError:
-                attributes[file_path][current_attribute] = parsed
+            key, val = list(get_key_val(line[:-1]).items())[0]
+            if current_attribute in attributes[file_path].keys():
+                if key in attributes[file_path][current_attribute].keys():
+                    attributes[file_path][current_attribute][key] += f";{val}"
+                else:
+                    attributes[file_path][current_attribute][key] = val
+            else:
+                attributes[file_path][current_attribute][key] = val
         else:
             raise ValueError(f"Unknown line format: {line}")
 
