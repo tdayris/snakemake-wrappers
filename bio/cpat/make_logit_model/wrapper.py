b57c8fd03ea8b273ab40b4221906faefe6d431aa
tdayris <tdayris@gustaveroussy.fr>
2020-08-20 13:03:55 +0200

    [dev] (make_logit_model): Initial commit

---
+++ bio/cpat/make_logit_model/wrapper.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""This is the Snakemake Wrapper for CPAT make_legit_model.py"""
+
+__author__ = "Thibault Dayris"
+__copyright__ = "Copyright 2020, Thibault Dayris"
+__email__ = "thibault.dayris@gustaveroussy.fr"
+__license__ = "MIT"
+
+
+from snakemake.shell import shell
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+
+extra = snakemake.params.get("extra", "")
+prefix = snakemake.output[0]
+
+reference = ""
+if "reference" in snakemake.input.keys():
+    reference = "--ref {snakemake.input['reference']}"
+
+shell(
+    " make_logitModel.py "
+    " --cgene {snakemake.input['coding']} "
+    " --ngene {snakemake.input['noncoding']} "
+    " --hex {snakemake.input['hexamer_table']} "
+    " --outfile {prefix} "
+    " {reference} "
+    " {extra} "
+    " {log} "
+)
