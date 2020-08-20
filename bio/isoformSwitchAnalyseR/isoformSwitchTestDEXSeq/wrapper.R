fe741c9b1a8f6b3c832b21f0113303906dfb7e68
tdayris <tdayris@gustaveroussy.fr>
2020-08-20 15:09:45 +0200

    [dev] (IsoformSwitchTestDEXSeq): Initial commit

---
+++ bio/isoformSwitchAnalyseR/isoformSwitchTestDEXSeq/wrapper.R
@@ -0,0 +1,48 @@
+#!/usr/bin/R
+
+# This script takes a IsoformSwitchAnalyseR object
+# and tests exons with differential expression
+
+# __author__ = "Thibault Dayris"
+# __copyright__ = "Copyright 2020, Thibault Dayris"
+# __email__ = "thibault.dayris@gustaveroussy.fr"
+# __license__ = "MIT"
+
+# The main package
+base::library(package = "isoformSwitchAnalyzeR", quietly = TRUE);
+base::library(package = "DEXSeq", quietly = TRUE);
+
+# Gathering input dataset and parameters
+switch_list <- base::as.character(
+  x = snakemake@input[["switch_list"]]
+);
+
+extra <- "switchAnalyzeRlist = switch_list";
+if ("extra" %in% base::anems(snakemake@params)) {
+  extra <- base::paste(
+      extra,
+      snakemake@params[["extra"]],
+      sep = ", "
+  );
+}
+
+command <- base::paste0(
+  "InsoformSwitchAnalyseR::isoformSwitchTestDEXSeq(",
+  extra,
+  ")"
+);
+base::message("Libraries and input data loaded");
+base::message(command);
+
+# Running command
+base::eval(
+  base::parse(
+    text = command
+  )
+);
+
+# Saving results
+base::saveRDS(
+  obj = edgn,
+  file = snakemake@output[["rds"]]
+);
