877219969b48e93278d2c9ba97201dbf88546a98
tdayris <tdayris@gustaveroussy.fr>
2020-09-21 09:04:36 +0200

    [dev] (Bowtie2-build): Initial commit

---
+++ bio/bowtie2/build/wrapper.py
@@ -0,0 +1,31 @@
+"""Snakemake wrapper for bowtie2 build"""
+
+__author__ = "Thibault Dayris"
+__copyright__ = "Copyright 2020"
+__email__ = "koester@jimmy.harvard.edu"
+__license__ = "MIT"
+
+
+from snakemake.shell import shell
+extra = snakemake.params.get("extra", "")
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+
+input = ""
+if "fasta" in snakemake.input.keys():
+    input = "-f {}".format(snakemake.input["fasta"])
+elif "fasta" in snakemake.params.keys():
+    input = "-c {}".format(snakemake.params["fasta"])
+else:
+    raise ValueError(
+        "Input sequence could not be found."
+    )
+
+prefix =
+
+shell(
+    " bowtie2-build "
+    " {input} "
+    " {snakemake.params['prefix']} "
+    " --threads {snakemake.threads} "
+    " {extra} "
+)
