9462c2c5dec4fc0cb3cbe7ab59fc63a8e7839598
tdayris <tdayris@gustaveroussy.fr>
2020-09-24 11:37:55 +0200

    [fix] (TypeError): Memory per threads

--- bio/samtools/sort/wrapper.py
+++ bio/samtools/sort/wrapper.py
@@ -18,7 +18,7 @@ threads = "" if snakemake.threads <= 1 else " -@ {} ".format(snakemake.threads -
 # Memory per thread
 extra = snakemake.params[0]
 if "mem_mb" in snakemake.resources.keys() and "-m" not in extra:
-    extra += " -m {}M".format(int(snakemake.resources["mem_mb"] / threads))
+    extra += " -m {}M".format(int(snakemake.resources["mem_mb"] / snakemake.threads))
 
 shell(
     "samtools sort {snakemake.params} {threads} -o {snakemake.output[0]} "
