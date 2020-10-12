3299dacce7147a31086538141b968fd8d7bd208b
tdayris <tdayris@gustaveroussy.fr>
2020-10-12 15:57:52 +0200

    [dev] (eacon): New tested wrapper

---
+++ bio/eacon/databases/wrapper.py
@@ -0,0 +1,19 @@
+#!/bin/python3.8
+
+# This wrapper downloads and install optional EaCoN packages
+
+__author__ = "Thibault Dayris"
+__copyright__ = "Copyright 2020, Thibault Dayris"
+__email__ = "thibault.dayris@gustaveroussy.fr"
+__license__ = "MIT"
+
+from snakemake.shell import shell
+log = snakemake.log_fmt_shell(stdout=True, stderr=True, append=True)
+
+if "databases" in snakemake.output.keys():
+    shell("wget -O {snakemake.output.databases}.tar.bz2 https://nextcloud.gustaveroussy.fr/s/E7P3oTKS2Lo2x87/download?path=Onco_Cyto/Annotations/EaCoN_databases.tar.bz2 {log}")
+    shell("tar -xvjf {snakemake.output[0]}.tar.bz2 {log}")
+
+if "grd" in snakemake.output.keys():
+    shell("wget -O {snakemake.output.grd} https://raw.githubusercontent.com/tdayris/cel-cnv-eacon/master/scripts/grd {log}")
+    shell("chmod u+x {snakemake.output.grd} {log}")
