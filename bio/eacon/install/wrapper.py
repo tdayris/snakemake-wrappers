4015d36129f920d8f106e38addf2f64fd59db516
tdayris <tdayris@gustaveroussy.fr>
2021-02-11 09:33:47 +0100

    [fix] (EaCoN): New http sources

--- bio/eacon/install/wrapper.py
+++ bio/eacon/install/wrapper.py
@@ -18,39 +18,44 @@ shell('R --vanilla -e "options(unzip = \'internal\'); Sys.setenv(TAR = \'$(which
 shell('R --vanilla -e \'options(unzip = "internal"); Sys.setenv(TAR = "$(which tar)"); devtools::install_github("gustaveroussy/apt.cytoscan.2.4.0"); \' {log}')
 
 if snakemake.params.get("OncoScan", True) is True:
-    shell('wget "https://partage.gustaveroussy.fr/pydio_public/b88fb8?dl=true&file=/OncoScan.na33.r4_0.1.0.tar.gz" -O {out_dir}/OncoScan.na33.r4_0.1.0.tar.gz {log}')
+    shell('wget "https://nextcloud.gustaveroussy.fr/s/jrWWJDdpJbaR82o/download" -O {out_dir}/OncoScan.na33.r4_0.1.0.tar.gz {log}')
     shell('R  --vanilla CMD INSTALL --clean --data-compress=gzip {out_dir}/OncoScan.na33.r4_0.1.0.tar.gz {log}')
 
-    shell('wget "https://partage.gustaveroussy.fr/pydio_public/582a03?dl=true&file=/OncoScan.na36.r1_0.1.0.tar.gz" -O {out_dir}/OncoScan.na36.r1_0.1.0.tar.gz {log}')
+    shell('wget "https://nextcloud.gustaveroussy.fr/s/ZpnYYwmKPzaeHWj/download" -O {out_dir}/OncoScan.na36.r1_0.1.0.tar.gz {log}')
     shell('R  --vanilla CMD INSTALL --clean --data-compress=gzip {out_dir}/OncoScan.na36.r1_0.1.0.tar.gz {log}')
 
 if snakemake.params.get("OncoScanCNV", True) is True:
-    shell('wget "https://partage.gustaveroussy.fr/pydio_public/cd59c8?dl=true&file=/OncoScanCNV.na33.r2_0.1.0.tar.gz" -O {out_dir}/OncoScanCNV.na33.r2_0.1.0.tar.gz {log}')
+    shell('wget "https://nextcloud.gustaveroussy.fr/s/BanRj6fAn4HFAA5/download" -O {out_dir}/OncoScanCNV.na33.r2_0.1.0.tar.gz {log}')
     shell('R  --vanilla CMD INSTALL --clean --data-compress=gzip {out_dir}/OncoScanCNV.na33.r2_0.1.0.tar.gz {log}')
 
-    shell('wget "https://partage.gustaveroussy.fr/pydio_public/41d8af?dl=true&file=/OncoScanCNV.na36.r1_0.1.0.tar.gz" -O {out_dir}/OncoScanCNV.na36.r1_0.1.0.tar.gz {log}')
+    shell('wget "https://nextcloud.gustaveroussy.fr/s/MQ9LwiZAHxnzJ2D/download" -O {out_dir}/OncoScanCNV.na36.r1_0.1.0.tar.gz {log}')
     shell('R  --vanilla CMD INSTALL --clean --data-compress=gzip {out_dir}/OncoScanCNV.na36.r1_0.1.0.tar.gz {log}')
 
 
 if snakemake.params.get("CytoScan750K", True) is True:
-    shell(f'wget "https://partage.gustaveroussy.fr/pydio_public/74d4cf?dl=true&file=/CytoScan750K.Array.na33.r4_0.1.0.tar.gz" -O {out_dir}/CytoScan750K.Array.na33.r4_0.1.0.tar.gz {log}')
+    shell('wget "https://nextcloud.gustaveroussy.fr/s/zx6iwPNKxX798Zg/download" -O {out_dir}/CytoScan750K.Array.na33.r4_0.1.0.tar.gz {log}')
     shell('R  --vanilla CMD INSTALL --clean --data-compress=gzip {out_dir}/CytoScan750K.Array.na33.r4_0.1.0.tar.gz {log}')
 
-    shell('wget "https://partage.gustaveroussy.fr/pydio_public/656d13?dl=true&file=/CytoScan750K.Array.na36.r1_0.1.0.tar.gz" -O {out_dir}/CytoScan750K.Array.na36.r1_0.1.0.tar.gz {log}')
+    shell('wget "https://nextcloud.gustaveroussy.fr/s/riXGCQNBENdkQSM/download" -O {out_dir}/CytoScan750K.Array.na36.r1_0.1.0.tar.gz {log}')
     shell('R  --vanilla CMD INSTALL --clean --data-compress=gzip {out_dir}/CytoScan750K.Array.na36.r1_0.1.0.tar.gz {log}')
 
 if snakemake.params.get("CytoScanHD", True) is True:
-    shell('wget "https://partage.gustaveroussy.fr/pydio_public/bc4b54?dl=true&file=/CytoScanHD.Array.na33.r4_0.1.0.tar.gz" -O {out_dir}/CytoScanHD.Array.na33.r4_0.1.0.tar.gz {log}')
+    shell('wget "https://nextcloud.gustaveroussy.fr/s/ip294gysJdcccYm/download" -O {out_dir}/CytoScanHD.Array.na33.r4_0.1.0.tar.gz {log}')
     shell('R  --vanilla CMD INSTALL --clean --data-compress=gzip {out_dir}/CytoScanHD.Array.na33.r4_0.1.0.tar.gz {log}')
 
-    shell('wget "https://partage.gustaveroussy.fr/pydio_public/24b026?dl=true&file=/CytoScanHD.Array.na36.r1_0.1.0.tar.gz" -O {out_dir}/CytoScanHD.Array.na36.r1_0.1.0.tar.gz {log}')
+    shell('wget "https://nextcloud.gustaveroussy.fr/s/SjRmBFreAee9mqD/download" -O {out_dir}/CytoScanHD.Array.na36.r1_0.1.0.tar.gz {log}')
     shell('R  --vanilla CMD INSTALL --clean --data-compress=gzip {out_dir}/CytoScanHD.Array.na36.r1_0.1.0.tar.gz {log}')
 
+if snakemake.params.get("genomewide", False) is True:
+    shell('R --vanilla CMD \'install_github("gustaveroussy/apt.snp6.1.20.0");\' {log}')
+    shell('wget "https://nextcloud.gustaveroussy.fr/s/46iyPjPPjFsni5S/download" -O {out_dir}/GenomeWideSNP.6.na35.r1_0.1.0.tar.gz {log}')
+    shell('R  --vanilla CMD INSTALL --clean --data-compress=gzip {out_dir}/GenomeWideSNP.6.na35.r1_0.1.0.tar.gz {log}')
+
 if snakemake.params.get("norm", True) is True:
-    shell('wget "https://partage.gustaveroussy.fr/pydio_public/e6fe22?dl=true&file=/rcnorm_0.1.5.tar.gz" -O {out_dir}/rcnorm_0.1.5.tar.gz {log}')
+    shell('wget "https://nextcloud.gustaveroussy.fr/s/Zc7JR3QaAk6rFBi/download" -O {out_dir}/rcnorm_0.1.5.tar.gz {log}')
     shell('R  --vanilla CMD INSTALL --clean --data-compress=gzip {out_dir}/rcnorm_0.1.5.tar.gz {log}')
 
-    shell('wget "https://partage.gustaveroussy.fr/pydio_public/083305?dl=true&file=/affy.CN.norm.data_0.1.2.tar.gz" -O {out_dir}/affy.CN.norm.data_0.1.2.tar.gz {log}')
+    shell('wget "https://nextcloud.gustaveroussy.fr/s/gfLnN8xrndFHMdM/download" -O {out_dir}/affy.CN.norm.data_0.1.2.tar.gz {log}')
     shell('R --vanilla CMD INSTALL --clean --data-compress=gzip {out_dir}/affy.CN.norm.data_0.1.2.tar.gz {log}')
 
 if snakemake.params.get("EaCoN_dev", False) is True:
