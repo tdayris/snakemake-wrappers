436c238ab660518c88848262c1ebacf832c20acc
tdayris <tdayris@gustaveroussy.fr>
2021-04-07 12:11:07 +0200

    [dev] (EaCoN): Complete IO

--- bio/eacon/segment/wrapper.R
+++ bio/eacon/segment/wrapper.R
@@ -1,5 +1,18 @@
 #!/bin/R
 
+# This is the snakemake wrapper for EaCoN segmentation
+
+# __author__ = "Thibault Dayris"
+# __copyright__ = "Copyright 2020, Thibault Dayris"
+# __email__ = "thibault.dayris@gustaveroussy.fr"
+# __license__ = "MIT"
+
+# Sink the stderr and stdout to the snakemake log file
+# https://stackoverflow.com/a/48173272
+log.file<-file(snakemake@log[[1]],open="wt");
+base::sink(log.file);
+base::sink(log.file,type="message");
+
 base::library(package = "EaCoN", quietly = TRUE);
 
 input_rds <- base::as.character(
@@ -23,3 +36,8 @@ base::eval(
     text=command
   )
 );
+
+# Proper syntax to close the connection for the log file
+# but could be optional for Snakemake wrapper
+base::sink(type="message");
+base::sink();
