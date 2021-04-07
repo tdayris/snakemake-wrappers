9b07388791f2b800562c060114a8e38e5259063d
tdayris <tdayris@gustaveroussy.fr>
2021-04-07 12:05:18 +0200

    [dev] (EaCoN): Now all output files are listed

--- bio/eacon/ascn/wrapper.R
+++ bio/eacon/ascn/wrapper.R
@@ -7,6 +7,12 @@
 # __email__ = "thibault.dayris@gustaveroussy.fr"
 # __license__ = "MIT"
 
+# Sink the stderr and stdout to the snakemake log file
+# https://stackoverflow.com/a/48173272
+log.file<-file(snakemake@log[[1]],open="wt");
+base::sink(log.file);
+base::sink(log.file,type="message");
+
 library(package = "EaCoN", quietly = TRUE);
 
 # Load user data
@@ -36,3 +42,8 @@ base::eval(
     text = command
   )
 );
+
+# Proper syntax to close the connection for the log file
+# but could be optional for Snakemake wrapper
+base::sink(type="message");
+base::sink();
