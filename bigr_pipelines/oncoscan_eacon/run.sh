58034f09eaf7f5f683a22b78da2a1747fa1602ba
tdayris <thibault.dayris@gustaveroussy.fr>
2021-04-16 09:01:10 +0200

    [doc] (OncoScans): Documentation fixes

--- bigr_pipelines/oncoscan_eacon/run.sh
+++ bigr_pipelines/oncoscan_eacon/run.sh
@@ -2,14 +2,15 @@ set -e
 
 # This adds several functions and variable in the environment
 PIPELINE_PREFIX="/mnt/beegfs/pipelines/snakemake-wrappers"
-source "${PIPELINE_PREFIX}/bigr_pipelines/common/bash/functions.sh"
+source "${PIPELINE_PREFIX}/bigr_pipelines/common/bash/messages.sh"
 source "${PIPELINE_PREFIX}/bigr_pipelines/common/bash/environment.sh"
 
 # Define pipeline related variables
-declare -x SNAKEMAKE_PROFILE_PATH="${PIPELINE_PREFIX}/bigr_pipelines/profile/clinics"
+declare -x SNAKEMAKE_PROFILE_PATH="${PIPELINE_PREFIX}/bigr_pipelines/common/profiles/clinics"
 declare -x PIPELINE_PATH="${PIPELINE_PREFIX}/meta/bio/eacon_oncoscan"
 export SNAKEMAKE_PROFILE_PATH PIPELINE_PATH
+message INFO "Environment loaded"
 
 # Run pipeline
-conda activate "${CONDA_ENV_PATH}" || error_handling "${LINENO}" 1 "Could not activate conda environment"
-snakemake -s "${PIPELINE_PATH}/Snakefile" --profile "${SNAKEMAKE_PROFILE_PATH}" || error_handling "${LINENO}" 2 "Error while running Cytoscan pipeline"
+conda_activate "${CONDA_ENV_PATH}" && message INFO "Conda loaded" || error_handling "${LINENO}" 1 "Could not activate conda environment"
+snakemake -s "${PIPELINE_PATH}/test/Snakefile" --cache eacon_install eacon_databases --profile "${SNAKEMAKE_PROFILE_PATH}" && message INFO "EaCoN successful" || error_handling "${LINENO}" 2 "Error while running Cytoscan pipeline"
