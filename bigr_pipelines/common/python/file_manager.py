bf198da790f9c16c81be6f57dae53603391a2b51
tdayris <tdayris@gustaveroussy.fr>
2021-04-30 15:58:46 +0200

    [dev] (common): New functions and return types

--- bigr_pipelines/common/python/file_manager.py
+++ bigr_pipelines/common/python/file_manager.py
@@ -6,35 +6,54 @@ This file contains usefull functions for file managment:
 * get a dictionnary from a basename (fasta)
 """
 
+import logging
 import pandas
+import os
+import shutil
 
 from pathlib import Path
 
-from os.path import commonprefix
+from os.path import commonprefix, basename
 from typing import Any, Callable, Optional, Union
 
+from write_yaml import write_yaml_from_path
+
 FilePathType = Union[str, Path]
+
+
+def remove_suffixes(word: str, suffixes: list[str]) -> str:
+    for suffix in suffixes:
+        if word.endswith(suffix):
+            return word[:-len(suffix)]
+    return word
+
 
 def get_fasta_index_from_genome_path(genome_path: str) -> str:
     return genome_path + ".fai"
 
+
 def get_fai(genome_path: str) -> str:
+
     return get_fasta_index_from_genome_path(genome_path)
 
 def get_fasta_dict_from_genome_path(genome_path: str) -> str:
     return ".".join(genome_path.split(".")[:-1]) + ".dict"
 
+
 def get_dict(genome_path: str) -> str:
     return get_fasta_dict_from_genome_path(genome_path)
+
 
 def get_vcf_tbi_from_vcf_path(vcf_path: str) -> str:
     return vcf_path + ".tbi"
 
+
 def get_tbi(vcf_path: str) -> str:
     return get_vcf_tbi_from_vcf_path(vcf_path)
 
+
 def search_files(dirpath: FilePathType, ext: Optional[str] = None) -> list[str]:
-    print("Searching in: {} for files ending with {}".format(dirpath, ext))
+    logging.info("Searching in: {} for files ending with {}".format(dirpath, ext))
     if isinstance(dirpath, str):
         dirpath = Path(dirpath)
 
@@ -50,35 +69,57 @@ def search_files(dirpath: FilePathType, ext: Optional[str] = None) -> list[str]:
             yield str(filepath.absolute())
 
 
-def search_vcf_files(dirpath: FilePathType) -> list[str]:
-    return search_files(dirpath, ext=("vcf", "vcf.gz", "bcf"))
+def search_vcf_files(dirpath: FilePathType) -> dict[str, str]:
+    suffixes = ["vcf", "vcf.gz", "bcf"]
+    return {
+        remove_suffixes(basename(vcf), suffixes): vcf
+        for vcf in search_files(dirpath, ext=suffixes)
+    }
 
-def search_vcf(dirpath: FilePathType) -> list[str]:
+
+def search_vcf(dirpath: FilePathType) -> dict[str, str]:
     return search_vcf_files(dirpath)
 
-def search_fastq_files(dirpath: FilePathType) -> list[str]:
-    return search_files(dirpath, ext=("fastq", "fq", "fastq.gz", "fq.gz"))
+
+def search_fastq_files(dirpath: FilePathType) -> dict[str, str]:
+    suffixes = ["fastq", "fq", "fastq.gz", "fq.gz"]
+    return {
+        remove_suffixes(basename(fastq), suffixes): fastq
+        for fastq in search_files(dirpath, ext=suffixes)
+    }
+
 
 def search_fastq_pairs(dirpath: FilePathType) -> dict[str, dict[str, str]]:
     return {
-        commonprefix([Path(r1).name, Path(r2).name]): {"Upstream_file": r1, "Downstream_file": r2}
-        for r1, r2 in zip(*[iter(search_fastq_files(dirpath))]*2)
+        commonprefix([Path(r1).name, Path(r2).name]): {
+            "Upstream_file": r1, "Downstream_file": r2
+        } for r1, r2 in zip(*[iter(search_fastq_files(dirpath))]*2)
     }
 
-def search_mapping(dirpath: FilePathType) -> list[str]:
-    return search_files(dirpath, ext=("sam", "bam", "cram"))
 
-def search_bam(dirpath: FilePathType) -> list[str]:
+def search_mapping(dirpath: FilePathType) -> dict[str, str]:
+    suffixes = ["sam", "bam", "cram"]
+    return {
+        remove_suffixes(basename(mapping), suffixes): mapping
+        for mapping in search_files(dirpath, ext=suffixes)
+    }
+
+
+def search_bam(dirpath: FilePathType) -> dict[str, str]:
     return search_mapping(dirpath)
+
 
 def search_fasta(dirpath: FilePathType) -> list[str]:
     return search_files(dirpath, ext=("fa", "fasta", "fa.gz", "fasta.gz"))
 
+
 def search_fa(dirpath: FilePathType) -> list[str]:
     return search_fasta(dirpath)
 
+
 def read_design(design_path: FilePathType) -> pandas.DataFrame:
     return pandas.read_csv(design_path, sep="\t", header=0)
+
 
 def build_design(
         dirpath: FilePathType,
@@ -92,13 +133,34 @@ def build_design(
     return design
 
 
-def build_config(
-        default_config: dict[str, Any],
-        default_config_path: FilePathType) -> dict[str, Any]:
-    if isinstance(default_config_path, str):
-        default_config_path = Path(default_config_path)
-
-    if default_config_path.exists():
-        print("Configfile provided.")
+def get_design(
+        dirpath: FilePathType,
+        search_fn: Callable[FilePathType, dict[str, dict[str, str]]]) \
+        -> pandas.DataFrame:
+    design = None
+    if (design_path := Path("design.tsv")).exists():
+        logging.info("Design file available: %s", str(design_path.absolute()))
+        design = read_design(design_path)
     else:
-        print("Missing config file, falling back to default parameters")
+        logging.info("Design file not found, looking for avilable input files")
+        design = build_design(dirpath, search_fn)
+    return design
+
+
+def get_config(default_config: dict[str, Any]) -> str:
+    if (config_path := Path("config.yaml")).exists():
+        logging.info("Config file available: %s", str(config_path.absolute()))
+    else:
+        logging.info(
+            "Generic config file not found, falling back to default arguments"
+        )
+        write_yaml_from_path(config_path, default_config)
+    return str(config_path.absolute())
+
+
+def design_config(
+        default_config: dict[str, Any],
+        dirpath: FilePathType,
+        search_fn: Callable[FilePathType, dict[str, dict[str, str]]]) \
+        -> list[Union[pandas.DataFrame, str]]:
+    return [get_design(dirpath, search_fn), get_config(default_config)]
--- bigr_pipelines/common/python/write_yaml.py
+++ bigr_pipelines/common/python/write_yaml.py
@@ -6,7 +6,7 @@ __email__ = "thibault.dayris@gustaveroussy.fr"
 __license__ = "MIT"
 
 """
-This script contains functions to read and write yaml files from various object 
+This script contains functions to read and write yaml files from various object
 types in python. Depending on the needs in the upcomming pipelines.
 """
 
@@ -28,7 +28,7 @@ def write_yaml(output_yaml: Union[str, Path], data: dict[str, Any]) -> None:
 
 def write_yaml_from_str(output_yaml: str, data: dict[str, Any]) -> None:
     """
-    Save given dictionnary as Yaml-formatted text file, 
+    Save given dictionnary as Yaml-formatted text file,
     the output_yaml is a string.
     """
     with open(output_yaml, "w") as outyaml:
@@ -42,3 +42,10 @@ def write_yaml_from_path(output_yaml: Path, data: dict[str, Any]) -> None:
     """
     with output_yaml.open("w") as outyaml:
         yaml.dump(data, outyaml, default_flow_style=False)
+
+
+def read_yaml(yaml_path: Union[str, Path]) -> dict[str, Any]:
+    res = {}
+    if Path(yaml_path).exists:
+        res = yaml.load(yaml_path)
+    return res
