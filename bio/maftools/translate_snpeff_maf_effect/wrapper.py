7eff24c86e924c7e0b0a759397b79d1a5311716b
tdayris <tdayris@gustaveroussy.fr>
2021-06-04 09:38:39 +0200

    [dev] (translate_snpeff): New words added

--- bio/maftools/translate_snpeff_maf_effect/wrapper.py
+++ bio/maftools/translate_snpeff_maf_effect/wrapper.py
@@ -23,7 +23,7 @@ logging.basicConfig(
     level=logging.DEBUG
 )
 
-log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+# log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
 accepted_terms = [
     "Frame_Shift_Del",
@@ -40,58 +40,78 @@ accepted_terms = [
 
 
 translation_dict = {
-    "Synonymous_Variant": "chromosome_number_variation",
-    "Splice_Site": "exon_loss_variant",
-    "Missense_Mutation": "frameshift_variant",
-    "Nonsense_Mutation": "stop_gained",
-    "Missense_Mutation": "stop_lost",
-    "Nonsense_Mutation": "start_lost",
-    "Splice_Site": "splice_acceptor_variant",
-    "Splice_Site": "splice_donor_variant",
-    "Missense_Mutation": "rare_amino_acid_variant",
-    "Missense_Mutation": "missense_variant",
-    "Frame_Shift_Ins": "disruptive_inframe_insertion",
-    "In_Frame_Ins": "conservative_inframe_insertion",
-    "Frame_Shift_Del": "disruptive_inframe_deletion",
-    "In_Frame_Del": "conservative_inframe_deletion",
-    "5_prime_UTR_variant": "5_prime_UTR_truncation+exon_loss_variant",
-    "3_prime_UTR_variant": "3_prime_UTR_truncation+exon_loss",
-    "Splice_Site": "splice_branch_variant",
-    "Splice_Site": "splice_region_variant",
-    "Nonsense_Mutation": "stop_retained_variant",
-    "Missense_Mutation": "initiator_codon_variant",
-    "Synonymous_Variant": "synonymous_variant",
-    "Missense_Mutation": "initiator_codon_variant+non_canonical_start_codon",
-    "Nonsense_Mutation": 'stop_retained_variant',
-    "Missense_Mutation": "coding_sequence_variant",
+    "chromosome_number_variation": "Synonymous_Variant",
+    "exon_loss_variant": "Splice_Site",
+    "frameshift_variant": "Missense_Mutation",
+    "stop_gained": "Nonsense_Mutation",
+    "stop_lost": "Missense_Mutation",
+    "start_lost": "Nonsense_Mutation",
+    "splice_acceptor_variant": "Splice_Site",
+    "splice_acceptor_variant&intron_variant": "Splice_Site",
+    "splice_region_variant&non_coding_transcript_exon_variant": "Splice_Site",
+    "splice_region_variant&synonymous_variant": "Splice_Site",
+    "splice_donor_variant&intron_variant": "Splice_Site",
+    "splice_donor_variant": "Splice_Site",
+    "structural_interaction_variant": "Missense_Mutation",
+    "rare_amino_acid_variant": "Missense_Mutation",
+    "missense_variant": "Missense_Mutation",
+    "missense_variant&splice_region_variant": "Splice_Site",
+    "disruptive_inframe_insertion": "Frame_Shift_Ins",
+    "conservative_inframe_insertion": "In_Frame_Ins",
+    "disruptive_inframe_deletion": "Frame_Shift_Del",
+    "conservative_inframe_deletion": "In_Frame_Del",
+    "5_prime_UTR_truncation+exon_loss_variant": "5_prime_UTR_variant",
+    "3_prime_UTR_truncation+exon_loss": "3_prime_UTR_variant",
+    "splice_branch_variant": "Splice_Site",
+    "splice_region_variant": "Splice_Site",
+    "stop_retained_variant": "Nonsense_Mutation",
+    "initiator_codon_variant": "Missense_Mutation",
+    "synonymous_variant": "Synonymous_Variant",
+    "initiator_codon_variant+non_canonical_start_codon": "Missense_Mutation",
+    'stop_retained_variant': "Nonsense_Mutation",
+    "coding_sequence_variant": "Missense_Mutation",
     "5_prime_UTR_variant": "5_prime_UTR_variant",
     "3_prime_UTR_variant": "3_prime_UTR_variant",
-    "5_prime_UTR_variant": "5_prime_UTR_premature_start_codon_gain_variant",
-    "Synonymous_Variant": "upstream_gene_variant",
-    "Synonymous_Variant": "downstream_gene_variant",
-    "Synonymous_Variant": "TF_binding_site_variant",
-    "Synonymous_Variant": "regulatory_region_variant",
-    "Synonymous_Variant": "miRNA",
-    "Synonymous_Variant": "custom",
-    "Missense_Mutation": "sequence_feature",
-    "Splice_Site": "conserved_intron_variant",
-    "Synonymous_Variant": "intron_variant",
-    "Synonymous_Variant": "intragenic_variant",
-    "Splice_Site": "conserved_intergenic_variant",
-    "Synonymous_Variant": "intergenic_region",
-    "5_prime_UTR_variant": "coding_sequence_variant",
-    "Synonymous_Variant": "non_coding_exon_variant",
-    "Synonymous_Variant": "nc_transcript_variant",
-    "5_prime_UTR_variant": "gene_variant",
-    "Synonymous_Variant": "chromosome"
+    "5_prime_UTR_premature_start_codon_gain_variant": "5_prime_UTR_variant",
+    "upstream_gene_variant": "Synonymous_Variant",
+    "downstream_gene_variant": "Synonymous_Variant",
+    "TF_binding_site_variant": "Synonymous_Variant",
+    "regulatory_region_variant": "Missense_Mutation",
+    "miRNA": "Synonymous_Variant",
+    "custom": "Synonymous_Variant",
+    "sequence_feature": "Missense_Mutation",
+    "conserved_intron_variant": "Splice_Site",
+    "intron_variant": "Synonymous_Variant",
+    "intragenic_variant": "Synonymous_Variant",
+    "conserved_intergenic_variant": "Splice_Site",
+    "intergenic_region": "Synonymous_Variant",
+    "coding_sequence_variant": "Missense_Mutation",
+    "non_coding_exon_variant": "Synonymous_Variant",
+    "non_coding_transcript_exon_variant": "Synonymous_Variant",
+    "nc_transcript_variant": "Synonymous_Variant",
+    "gene_variant": "Missense_Mutation",
+    "chromosome": "Synonymous_Variant",
+    "Variant_Classification": "Variant_Classification",
+    "protein_protein_contact": "Missense_Mutation",
+    'splice_region_variant&intron_variant': "Splice_Site"
 }
 
-for idx, (key, value) in enumerate(translation_dict.items()):
-    replacement = "'s/\t{value}\t/\t{key}\t/g'"
-    if idx == 0:
-        shell("sed {replacement} {snakemake.input.maf} > {snakemake.input.maf}.tmp {log}")
-    else:
-        shell("sed -i {replacement} {snakemake.input.maf}.tmp {log}")
+logging.debug("Reading MAF file")
+df = pandas.read_csv(snakemake.input["maf"], sep="\t", header=0, index_col=None)
+logging.debug("Maf file loaded")
+df["Variant_Classification"] = [
+    translation_dict[v] for v in df["Variant_Classification"]
+]
+logging.debug("Variant_Classification translated")
+df.to_csv(snakemake.output["maf"], sep="\t", index=False)
+logging.debug("Variant_Classification translated")
 
-log = snakemake.log_fmt_shell(stdout=True, stderr=True)
-shell("mv -v {snakemake.input.maf}.tmp {snakemake.output.maf} {log}")
+# for idx, (key, value) in enumerate(translation_dict.items()):
+#     replacement = "'s/{value}/{key}/g'"
+#     if idx == 0:
+#         shell("sed {replacement} {snakemake.input.maf} > {snakemake.input.maf}.tmp {log}")
+#     else:
+#         shell("sed -i {replacement} {snakemake.input.maf}.tmp {log}")
+#
+# log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+# shell("mv -v {snakemake.input.maf}.tmp {snakemake.output.maf} {log}")
