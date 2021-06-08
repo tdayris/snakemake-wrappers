8828bdf496b3d723185b56842220721e494cd824
tdayris <tdayris@gustaveroussy.fr>
2021-06-08 10:04:43 +0200

    [dev] (eacon): Oncoscan almost local

--- bigr_pipelines/oncoscan_eacon/run.sh
+++ bigr_pipelines/oncoscan_eacon/run.sh
@@ -7,10 +7,10 @@ source "${PIPELINE_PREFIX}/bigr_pipelines/common/bash/environment.sh"
 
 # Define pipeline related variables
 declare -x SNAKEMAKE_PROFILE_PATH="${PIPELINE_PREFIX}/bigr_pipelines/common/profiles/clinics"
-declare -x PIPELINE_PATH="${PIPELINE_PREFIX}/meta/bio/eacon_oncoscan"
+declare -x PIPELINE_PATH="${PIPELINE_PREFIX}/bigr_pipelines/oncoscan_eacon/Snakefile"
 export SNAKEMAKE_PROFILE_PATH PIPELINE_PATH
 message INFO "Environment loaded"
 
 # Run pipeline
 conda_activate "${CONDA_ENV_PATH}" && message INFO "Conda loaded" || error_handling "${LINENO}" 1 "Could not activate conda environment"
-snakemake -s "${PIPELINE_PATH}/test/Snakefile" --profile "${SNAKEMAKE_PROFILE_PATH}" --cache eacon_install post_process_eacon_databases eacon_databases && message INFO "EaCoN successful" || error_handling "${LINENO}" 2 "Error while running Oncoscan pipeline"
+snakemake -s "${PIPELINE_PATH}" --profile "${SNAKEMAKE_PROFILE_PATH}" --cache eacon_install post_process_eacon_databases eacon_databases && message INFO "EaCoN successful" || error_handling "${LINENO}" 2 "Error while running Oncoscan pipeline"
