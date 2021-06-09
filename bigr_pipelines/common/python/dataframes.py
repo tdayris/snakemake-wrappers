caf04a19f4b4b0f09d288d97df4254db7396a7e9
tdayris <tdayris@gustaveroussy.fr>
2021-06-09 14:14:16 +0200

    [dev] (common): dataframe manipulations

---
+++ bigr_pipelines/common/python/dataframes.py
@@ -0,0 +1,54 @@
+"""
+This file contains usefull functions for pandas dataframe managment
+"""
+
+import pandas
+import logging
+
+from typing import Optional, Union
+
+def filter_df(df: pandas.DataFrame,
+              colname: str,
+              levels: list[str]) -> pandas.DataFrame:
+    """
+    Filter a dataframe given a column name and a list of levels to keep
+    """
+    return df[df[colname].isin(levels)]
+
+
+def yield_comps(complete_design: pandas.DataFrames,
+                aggregate: Optional[list[str]] = None,
+                remove: Optional[list[str]] = None) \
+                -> list[Union[pandas.DataFrame, list[str]]]:
+    """
+    Split a large design into simple ones and provide comparison information
+    """
+    if aggregate is not None:
+        for cols in aggregate:
+            complete_design["_".join(cols)] = (
+                complete_design[cols].astype(str)
+                                     .apply("_".join, axis=1)
+                                     .str.replace(" ", "_")
+                                     .str.strip()
+            )
+    if remove is not None:
+        complete_design.drop(remove, axis=1, inplace=True)
+
+    for col in complete_design.columns:
+        levels = sorted(
+            k for k, v in df[i].value_counts().items() if v > 1
+        )
+
+        if len(levels) <= 1:
+            # We need at least two levels to perfom a differential analysis
+            continue
+
+
+        for l1, l2 in combinations(levels, 2):
+            # R and DESeq2 sort levels through alphanumerical order. We have
+            # to provide user-understandable name ; so let us sort out the
+            # levels and guess reference name.
+            level, ref = sorted(l1, l2)
+
+            # Building humand readable design name
+            yield [col, level, ref]
