79aa3211746cdf0c07ea194470d8b113873a2e30
tdayris <tdayris@gustaveroussy.fr>
2021-06-09 15:27:11 +0200

    [fix] (salmon): syntax error

--- bigr_pipelines/salmon_quant/run.sh
+++ bigr_pipelines/salmon_quant/run.sh
@@ -11,19 +11,18 @@ declare -x PIPELINE_PATH="${PIPELINE_PREFIX}/bigr_pipelines/salmon_quant"
 export SNAKEMAKE_PROFILE_PATH PIPELINE_PATH
 
 SNAKEFILE_PATH="${PIPELINE_PATH}/Snakefile"
-CONFIG_PATH="${PIPELINE_PATH}/config.hg19.yaml"
-if [ "${1}" = "hg38" ]; then
-  CONFIG_PATH="${PIPELINE_PATH}/config.hg38.yaml"
-  message WARNING "HG38 has not been tested."
+CONFIG_PATH="${PIPELINE_PATH}/config.hg38.yaml"
+if [ "${1}" = "hg19" ]; then
+  CONFIG_PATH="${PIPELINE_PATH}/config.hg19.yaml"
+  message INFO "Using HG19"
 fi
 message INFO "Environment loaded"
 
-if [ ! -f "config.yaml"]; then
+if [ ! -f "config.yaml" ]; then
   rsync -cv "${CONFIG_PATH}" "config.yaml"
 else
   message INFO "Config file already provided"
 fi
-
 
 # Run pipeline
 conda_activate "${CONDA_ENV_PATH}" && message INFO "Conda loaded" || error_handling "${LINENO}" 1 "Could not activate conda environment"
