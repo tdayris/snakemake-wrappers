336864c10b12ce837f6a3eb65e7bdb5310b43aff
tdayris <tdayris@gustaveroussy.fr>
2021-06-21 16:57:22 +0200

    [dev] (variant_calling_germline): initial commit

---
+++ bigr_pipelines/variant_callling_germline/test.sh
@@ -0,0 +1,1 @@
+cd /mnt/beegfs/pipelines/snakemake-wrappers/bigr_pipelines/variant_calling_ampliseq/test/ && bash /mnt/beegfs/pipelines/snakemake-wrappers/bigr_pipelines/variant_calling_ampliseq/run.sh && cd -
