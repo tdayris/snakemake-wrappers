31e7ac3ae97e27c84dfae0122b7e64bb705e7fac
tdayris <thibault.dayris@gustaveroussy.fr>
2021-06-28 17:14:14 +0200

    [dev] (somatic): Somatic pipeline development

---
+++ bigr_pipelines/variant_calling_somatic/run.sh
@@ -0,0 +1,36 @@
+set -e
+
+# This adds several functions and variable in the environment
+PIPELINE_PREFIX="/mnt/beegfs/pipelines/snakemake-wrappers"
+source "${PIPELINE_PREFIX}/bigr_pipelines/common/bash/messages.sh"
+source "${PIPELINE_PREFIX}/bigr_pipelines/common/bash/environment.sh"
+
+# Define pipeline related variables
+declare -x SNAKEMAKE_PROFILE_PATH="${PIPELINE_PREFIX}/bigr_pipelines/common/profiles/slurm"
+declare -x PIPELINE_PATH="${PIPELINE_PREFIX}/bigr_pipelines/variant_calling_somatic"
+export SNAKEMAKE_PROFILE_PATH PIPELINE_PATH
+
+SNAKEFILE_PATH="${PIPELINE_PATH}/Snakefile"
+CONFIG_PATH="${PIPELINE_PATH}/config.hg38.yaml"
+SNAKE_ARGS=()
+
+while [ "$#" -gt 0 ]; do
+  case "${1}" in
+    hg19|HG19) CONFIG_PATH="${PIPELINE_PATH}/config.hg19.yaml"; shift;;
+    hg38|HG38) CONFIG_PATH="${PIPELINE_PATH}/config.hg38.yaml"; shift;;
+    *) SNAKE_ARGS+=("${1}"); shift;;
+  esac
+done
+message INFO "Environment loaded"
+
+if [ ! -f "config.yaml" ]; then
+  message INFO "Missing config file."
+  message INFO "Falling back to default arguments: ${CONFIG_PATH}"
+  rsync -v "${CONFIG_PATH}" "config.yaml"
+else
+  message INFO "Config file already provided"
+fi
+
+# Run pipeline
+conda_activate "${CONDA_ENV_PATH}" && message INFO "Conda loaded" || error_handling "${LINENO}" 1 "Could not activate conda environment"
+snakemake -s "${SNAKEFILE_PATH}" --configfile "config.yaml" --cache bwa_fixmate_meta_bwa_index bwa_index --profile "${SNAKEMAKE_PROFILE_PATH}" "${SNAKE_ARGS[@]}" && message INFO "Variant calling successful" || error_handling "${LINENO}" 2 "Error while running variant calling pipeline"
