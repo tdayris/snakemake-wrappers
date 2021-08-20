3bdf82b644669a41db992046bccd85466145616c
tdayris <thibault.dayris@gustaveroussy.fr>
2021-08-20 08:34:51 +0200

    [fix] (facets): VCF now optional and NameError fixed

--- bio/facets/cnv/wrapper.py
+++ bio/facets/cnv/wrapper.py
@@ -13,9 +13,13 @@ __license__ = "MIT"
 from snakemake.shell import shell
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
-extra = params.get("extra", "")
+extra = snakemake.params.get("extra", "")
 if "bed" in snakemake.input:
     extra += " --targets {input.bed}"
+    
+vcf = ""
+if "vcf" in snakemake.input.keys():
+    vcf = f"--snp-vcf {snakemake.input.vcf} "
 
 prefix = snakemake.output["vcf"][:-len(".vcf.gz")]
 
@@ -29,19 +33,21 @@ if ("--no-cov-plot" in extra) and ("coverage" in snakemake.output.keys()):
 if all(i in snakemake.input.keys() for i in ["tumor_bam", "normal_bam", "vcf"]):
     shell(
         "cnv_facets.R "
-        "--snp-tumour {input.tumor_bam} "
-        "--snp-normal {input.normal_bam} "
-        "--snp-vcf {input.vcf} "
+        "--snp-tumour {snakemake.input.tumor_bam} "
+        "--snp-normal {snakemake.input.normal_bam} "
+        "{vcf} "
         "--out {prefix} "
-        "--snp-nprocs {threads} "
+        "--snp-nprocs {snakemake.threads} "
         "{extra} "
         "{log}"
     )
 elif "pileup" in snakemake.input.keys():
     shell(
         "cnv_facets.R "
-        "--pileup {input.pileup} "
-        "--out {output.prefix} "
+        "--pileup {snakemake.input.pileup} "
+        "--out {snakemake.output.prefix} "
+        "--snp-nprocs {snakemake.threads} "
+        "{vcf} "
         "{extra} "
         "{log}"
     )
