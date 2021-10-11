b7c612ab36cebc671b206e677ee8feea079ce058
tdayris <thibault.dayris@gustaveroussy.fr>
2021-10-11 16:36:19 +0200

    [dev] (maftools): New tested wrapper

---
+++ bio/maftools/lollipop_plot/wrapper.R
@@ -0,0 +1,52 @@
+#!/usr/bin/R
+
+# __author__ = "Thibault Dayris"
+# __copyright__ = "Copyright 2020, Thibault Dayris"
+# __email__ = "thibault.dayris@gustaveroussy.fr"
+# __license__ = "MIT"
+
+# Snakemake wrapper for lollipop plot
+
+base::library(package = "maftools", quietly = TRUE);
+
+# Building graphics environment command
+png_extra <- base::paste0(
+  "filename", "='", base::as.character(x = snakemake@output[["png"]]), "'"
+);
+if ("png_extra" %in% base::names(snakemake@params)) {
+  png_extra <- base::paste(
+    png_extra, snakemake@params[["png_extra"]], sep = ", "
+  );
+}
+
+png_cmd_line <- base::paste0("grDevices::png(", png_extra, ")");
+
+# Building trinucleotideMatrix computation command line
+maf_obj <- base::readRDS(
+  file = base::as.character(x = snakemake@input[["maf"]])
+);
+
+lollipop_plot_extra = base::paste0(
+  "maf=maf_obj, gene='",
+  base::as.character(x=snakemake@wildcards[["gene"]]),
+  "'"
+);
+
+if ("lollipop_plot_extra" %in% base::names(snakemake@params)) {
+  lollipop_plot_extra <- base::paste(
+    lollipop_plot_extra, snakemake@params[["lollipop_plot_extra"]],
+    sep = ", "
+  );
+}
+lollipop_plot_cmd_line <- base::paste0(
+  "maftools::lollipopPlot(", lollipop_plot_extra, ")"
+);
+
+# Plotting signatures
+if ("png" %in% base::names(snakemake@output)) {
+  base::message(png_cmd_line);
+  base::message(lollipop_plot_cmd_line);
+  base::eval(base::parse(text = png_cmd_line));
+  base::eval(base::parse(text = lollipop_plot_cmd_line));
+  dev.off();
+}
