7e32aa72b2980d1cca97ff28380d44a115a873ad
tdayris <tdayris@gustaveroussy.fr>
2022-02-15 09:14:28 +0100

    [dev] (samtools): BED and FAI files now recognized

--- bio/samtools/view/wrapper.py
+++ bio/samtools/view/wrapper.py
@@ -15,8 +15,15 @@ ref = ""
 if "fasta" in snakemake.input.keys():
     ref = f"--reference {snakemake.input['fasta']}"
 
+if "fasta_idx" in snakemake.input.keys():
+    ref += f" --fai-reference {snakemake.input['fasta_idx']}"
+
+bed = ""
+if "bed" in snakemake.input.keys():
+    bed = f"--target-file {snakemake.input['bed']}"
+
 shell(
-    "samtools view {snakemake.params.extra} {ref} {samtools_opts} "
+    "samtools view {snakemake.params.extra} {ref} {bed} {samtools_opts} "
     "-o {snakemake.output[0]} {snakemake.input['aln']} "
     "{snakemake.params.position} {log}"
 )
