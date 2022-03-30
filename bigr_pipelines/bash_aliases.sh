f05ab193cfa8d6cf4605e5540660612293bd3dac
tdayris <thibault.dayris@outlook.fr>
2022-03-30 16:07:16 +0200

    [dev] (bash_aliases): Initial commit

---
+++ bigr_pipelines/bash_aliases.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Source this file to add new pipelines to your
+# environment. No more loooong command lines,
+# only the best.
+
+
+alias bigr_salmon_quant='bash "/mnt/beegfs/pipelines/snakemake-wrappers/bigr_pipelines/salmon_quant/run.sh"'
+alias bigr_ampliseq='bash "/mnt/beegfs/pipelines/snakemake-wrappers/bigr_pipelines/variant_calling_ampliseq/run.sh"'
+alias bigr_oncoscan='bash "/mnt/beegfs/pipelines/snakemake-wrappers/bigr_pipelines/oncoscan_eacon/run.sh"'
+alias bigr_cytoscan='bash "/mnt/beegfs/pipelines/snakemake-wrappers/bigr_pipelines/cytoscan_eacon/run.sh"'
