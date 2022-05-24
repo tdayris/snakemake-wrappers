6e75d983f34cd623b7bccae666ca64de91318920
tdayris <thibault.dayris@gustaveroussy.fr>
2022-05-24 11:37:48 +0200

    [fix] (readlink): Resolved, Absolute path used instead of user given path

--- bigr_pipelines/salmon_quant/pirest_run.sh
+++ bigr_pipelines/salmon_quant/pirest_run.sh
@@ -4,5 +4,7 @@
 # It call the usual run.sh file with parameters
 
 genome=`cut -f4 design.tsv | grep -P "GRC" | sort | uniq | sed 's/"//g'`
+script_dir=$(dirname $0)
+script_dir=$(readlink -e "${script_dir}")
 
-bash /mnt/beegfs/pipelines/snakemake-wrappers/bigr_pipelines/salmon_quant/run.sh "${genome}" --nt --rerun-incomplete
+bash "${script_dir}/run.sh" "${genome}" --nt --rerun-incomplete
