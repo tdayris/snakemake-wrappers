dac9065a3b2534a8cd9a3e914ca15510504f9c2e
tdayris <thibault.dayris@gustaveroussy.fr>
2022-08-12 22:16:16 +0200

    allow almost empty annotation

--- bio/BiGR/gleaves_compatibility/wrapper.py
+++ bio/BiGR/gleaves_compatibility/wrapper.py
@@ -95,50 +95,51 @@ def parse_info(chrom: str, pos: int, ref: str, alt: str, info: str) -> Dict[str,
     try:
         new_annotations["RRC"] = new_annotations["RRCp"] + new_annotations["RRCm"]
     except TypeError:
-        pass
+        new_annotations["RRC"] = None
 
 
     try:
         new_annotations["ARC"] = new_annotations["ARCp"] + new_annotations["ARCm"]
     except TypeError:
-        pass
+        new_annotations["ARC"] = None
 
     try:
         new_annotations["BRC"] = new_annotations["TRC"] - (new_annotations["RRC"] + new_annotations["ARC"])
     except TypeError:
-        pass
+        new_annotations["BRC"] = None
 
     try:
         new_annotations["BRR"] = new_annotations["BRC"] / new_annotations["TRC"]
     except TypeError:
-        pass
+        new_annotations["BRR"] = None
 
     try:
         new_annotations["BRE"] = 1 - new_annotations["BRR"]
     except TypeError:
-        pass
+        new_annotations["BRE"] = None
 
 
     new_annotations["BKG"] = get_bkg(new_annotations["BRE"])
     new_annotations["OLD_MULTIALLELIC"] = f"{chrom}:{pos}:{ref}:{alt}"
     new_annotations["OLD_VARIANT"] = f"{chrom}:{pos}:{ref}:{alt}"
     return new_annotations
-        
 
 
-def get_sb_table(sb_table: str) -> Dict[str, int]:
+def get_sb_table(sb_table: str) -> Dict[str, Optional[int]]:
     """Parse INFO field and return strand bias table"""
+    result = {"ARCp": None, "ARCm": None, "RRCp": None, "RRCm": None}
     try:
         sb_table_val = sb_table.split("=")[-1]
         alt, ref = sb_table_val.split("|")
         ARCp, ARCm = map(int, alt.split(","))
         RRCp, RRCm = map(int, ref.split(","))
-        return {"ARCp": ARCp, "ARCm": ARCm, "RRCp": RRCp, "RRCm": RRCm}
+        result = {"ARCp": ARCp, "ARCm": ARCm, "RRCp": RRCp, "RRCm": RRCm}
     except Exception as e:
         logging.error(sb_table)
         logging.error(sb_table_val)
         logging.error(e)
-        raise
+    finally:
+        return result
 
 
 def get_SOR(sor_field: str) -> Dict[str, float]:
