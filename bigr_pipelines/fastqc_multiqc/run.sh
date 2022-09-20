72fdbf6bd8efe6d8faafb4c65ae479e566caa828
tdayris <thibault.dayris@gustaveroussy.fr>
2022-09-20 16:50:53 +0200

    rnaseq like wrapper path definition

--- bigr_pipelines/fastqc_multiqc/run.sh
+++ bigr_pipelines/fastqc_multiqc/run.sh
@@ -3,6 +3,7 @@ set -e
 
 # This adds several functions and variable in the environment
 PIPELINE_PREFIX=$(readlink -e "$(dirname ${0})/../..")
+WRAPPERS_PATH=$(readlink -e "${PIPELINE_PATH}/../../")
 # shellcheck source=/mnt/beegfs/pipelines/snakemake-wrappers/bigr_pipelines/common/bash/messages.sh
 source "${PIPELINE_PREFIX}/bigr_pipelines/common/bash/messages.sh"
 # shellcheck source=/mnt/beegfs/pipelines/snakemake-wrappers/bigr_pipelines/common/bash/environment.sh
