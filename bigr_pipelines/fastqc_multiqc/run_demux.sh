d133e44eb6548be2d210c905fcc798cd12a7f002
tdayris <tdayris@gustaveroussy.fr>
2022-09-21 09:49:30 +0200

    source a conda if there is none

--- bigr_pipelines/fastqc_multiqc/run_demux.sh
+++ bigr_pipelines/fastqc_multiqc/run_demux.sh
@@ -3,4 +3,9 @@
 script_dir=$(dirname $0)
 script_dir=$(readlink -e "${script_dir}")
 
+if [[ ! "${PATH}" =~ conda ]];
+    # If there is no available conda, use mine
+    source "/mnt/beegfs/userdata/t_dayris/anaconda3/etc/profile.d/conda.sh"
+fi
+
 bash "${script_dir}/run.sh" -p demux output/multiqc.html "$@"
