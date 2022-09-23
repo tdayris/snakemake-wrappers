be8ebbad9de4555e598f87b5d3114110584d6df5
tdayris <tdayris@gustaveroussy.fr>
2022-09-23 15:09:55 +0200

    auto project mounter

---
+++ bigr_pipelines/common/bash/prepare_design.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+set -e
+
+# This adds several functions and variable in the environment
+PIPELINE_PREFIX=$(readlink -e "$(dirname ${0})")
+source "${PIPELINE_PREFIX}/messages.sh"
+source "${PIPELINE_PREFIX}/environment.sh"
+
+message WARNING "This script is interactive, do not leave."
+message INFO "I need your password to access datasets..."
+COMMAND="iinit"
+message CMD "${COMMAND}"
+eval ${COMMAND}
+
+
+message INFO "I search information about the project ${1}"
+
+COMMAND='imeta qu -C "${1}" | grep -v "\-\-\-" | cut -f2 -d" " > datasetList'
+message CMD "${COMMAND}"
+eval ${COMMAND}
+message INFO "I have found $(wc -l datasetList) datasets (including re-sequencing)"
+
+while true; do
+    read -p "Do you feel like its ok to continue ? (y/n) " yn
+
+    case "${yn}" in
+        [yY] ) message INFO "Ok, I shall proceeed"; break;;
+        [nN] ) message ERROR "Exiting..."; exit ;;
+        * ) message ERROR "Unknown response.";;
+    esac
+done
+
+
+my_iquest() {iquest "%s/%s" "SELECT COLL_NAME,DATA_NAME WHERE COLL_NAME like '${1}%'"}
+export -f my_iquest
+COMMAND="bash ${PIPELINE_PREFIX}/multiprocess_bash_command.sh -p ${MAX_THREADS} -f my_iquest $(cat datasetList | tr $'\n' ' ') > dataset_paths.txt"
+message CMD "${COMMAND}"
+eval ${COMMAND}
+
+
+while true; do
+    read -p "Is this dataset paried ? (y/n) " yn
+
+    case "${yn}" in
+        [yY] ) message INFO "Ok, I will pair fastq files alphabetically"; break;;
+        [nN] ) message ERROR "Nothing more to do."; exit ;;
+        * ) message ERROR "Unknown response.";;
+    esac
+done
+
+
+COMMAND='grep -vP ".*I[1,2]_001\..*\.gz" dataset_paths.txt | sort | uniq | paste - - > paired_dataset_paths.txt'
+message CMD "${COMMAND}"
+eval ${COMMAND}
+
+while true; do
+    read -p "Should I try to guess resequenced samples ? (y/n) " yn
+
+    case "${yn}" in
+        [yY] ) message INFO "Ok, I will guess multi-sequenced samples"; break;;
+        [nN] ) message ERROR "Nothing more to do."; exit ;;
+        * ) message ERROR "Unknown response.";;
+    esac
+done
+
+message INFO "There is a preview of your paired fastq files."
+COMMAND="head -n3 paired_dataset_paths.txt"
+message CMD "${COMMAND}"
+eval ${COMMAND}
+
+read -p "Provide a space separated list of suffixes to remove from fastq file names to guess multiple sequencing (regex allowed, order does not matter): " regex_list
+COMMAND="python3 ${PIPELINE_PATH}/../pyton/pair_guesser.py -e ${regex_list}"
+message CMD "${COMMAND}"
+eval ${COMMAND}
+
+message INFO "Process over."
---
+++ bigr_pipelines/common/python/pair_guesser.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""Read paired dataset paths from odin kdi procedures, and guess splitted samples"""
+
+import argparse
+import logging
+import re
+import sys
+
+from typing import Any, Dict, List, Optional
+
+
+def cmd_parser(args: Any) -> argparse.Namespace:
+    """Parse command line arguments"""
+    logging.info("Parsing command line...")
+    parser = argparse.ArgumentParser(
+        description="",
+        epilog="This script does not perform any magic. Check results."
+    )
+
+    parser.add_argument(
+        "-i", "--input",
+        help="Path to input TSV file",
+        default="paired_dataset_paths.txt",
+        type=str
+    )
+
+    parser.add_argument(
+        "-d", "--design",
+        help="Path to the output design file with merged fastq files",
+        default="design.tsv",
+        type=str
+    )
+
+    parser.add_argument(
+        "-e", "--extension",
+        help="Space separated list of suffixes to remove to guess sample name",
+        default=[".gz", ".fq", ".fastq", "_001", "_R1"],
+        nargs="+"
+    )
+
+    return parser.parse_args(args)
+
+
+def try_remove_ext(filename: str, possible_exts: Optional[List[str]] = None) -> str:
+    """From  a list of possible extensions, remove them one after each other"""
+    if possible_exts is None:
+        possible_exts = [
+            ".gz",
+            ".fastq",
+            ".fq",
+            "_001",
+            "_R1",
+        ]
+
+    for ext in possible_exts:
+        filename = re.sub(ext, "", filename)
+    return filename
+
+
+def index_paired_dataset(path: str, possible_exts: Optional[List[str]]) -> Dict[str, Dict[str, List[str]]]:
+    logging.info("Indexing input file...")
+    result = {}
+    with open(path, "r") as paired_dataset:
+        for line in paired_dataset:
+            left, right = line[:-1].split("\t")
+            sample_name = try_remove_ext(left.split("/")[-1], possible_exts)
+            try:
+                result[sample_name]["R1"].append(left)
+                result[sample_name]["R2"].append(right)
+            except KeyError:
+                result[sample_name] = {"R1": [left], "R2": [right]}
+
+    return result
+
+
+def create_design_file(path: str, index: Dict[str, Dict[str, List[str]]]) -> None:
+    logging.info("Saving design file...")
+    with open(path, "w") as design:
+        design.write("Sample_id\tUpstream_file\tDownstream_file\n")
+        for sample in index.keys():
+            if len(index[sample]["R1"]) > 1:
+                logging.info("{} seems to be divided in {} files".format(sample, len(index[sample]["R1"])))
+            line = "\t".join([
+                sample,
+                ",".join(index[sample]["R1"]),
+                ",".join(index[sample]["R2"]),
+            ]) + "\n"
+            design.write(line)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        # filename=snakemake.log[0],
+        # filemode="w",
+        level=logging.DEBUG
+    )
+
+    cmd = cmd_parser(sys.argv[1:])
+    logging.debug(cmd)
+
+    indexed_input = index_paired_dataset(cmd.input, cmd.extension)
+
+    create_design_file(cmd.design, indexed_input)
