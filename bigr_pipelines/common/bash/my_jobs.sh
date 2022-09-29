742f9ce224c6610b29e97bf1266ad9e9981adc4d
tdayris <tdayris@gustaveroussy.fr>
2022-09-29 09:17:17 +0200

    update colors

--- bigr_pipelines/common/bash/my_jobs.sh
+++ bigr_pipelines/common/bash/my_jobs.sh
@@ -2,6 +2,6 @@
 set -e
 
 squeue_format='%.10i %.9P %.35j %.8u %.8T %.10M %.10l %.6D %.3C %.10m %R'
-sed_colors='s/RUNNING/\e[32mRUNNING\e[0m/g;s/PENDING/\e[32mPENDING\e[0m/g;'
+sed_colors='s/RUNNING/\033[1;36mRUNNING\033[0m/g;s/PENDING/\033[1;33mPENDING\033[0m/g;s/COMPLETED/\033[1;32mCOMPLETED\033[0m/g;'
 
 squeue -o "${squeue_format}" -S j -u "${USER}" | sed "${sed_colors}"
