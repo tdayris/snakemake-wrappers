363ffbc08409a7f84944c9cd6da3b89425d43eb0
tdayris <tdayris@gustaveroussy.fr>
2022-09-29 09:23:12 +0200

    use hexadecimal

--- bigr_pipelines/common/bash/my_jobs.sh
+++ bigr_pipelines/common/bash/my_jobs.sh
@@ -2,6 +2,6 @@
 set -e
 
 squeue_format='%.10i %.9P %.35j %.8u %.8T %.10M %.10l %.6D %.3C %.10m %R'
-sed_colors='s/RUNNING/\033[1;36mRUNNING\033[0m/g;s/PENDING/\033[1;33mPENDING\033[0m/g;s/COMPLETED/\033[1;32mCOMPLETED\033[0m/g;'
+sed_colors='s/RUNNING/\x1b[1;36mRUNNING\x1b[0m/g;s/PENDING/\x1b[1;33mPENDING\x1b[0m/g;s/COMPLETED/\x1b[1;32mCOMPLETED\x1b[0m/g;'
 
 squeue -o "${squeue_format}" -S j -u "${USER}" | sed "${sed_colors}"
