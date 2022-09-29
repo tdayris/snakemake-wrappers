540c04043dcbd271eaca5e8b59ed7128a4c5749f
tdayris <tdayris@gustaveroussy.fr>
2022-09-29 09:28:41 +0200

    change order

--- bigr_pipelines/common/bash/my_jobs.sh
+++ bigr_pipelines/common/bash/my_jobs.sh
@@ -4,4 +4,4 @@ set -e
 squeue_format='%.10i %.9P %.35j %.8u %.8T %.10M %.10l %.6D %.3C %.10m %R'
 sed_colors='s/RUNNING/\x1b[1;36mRUNNING\x1b[0m/g;s/PENDING/\x1b[1;33mPENDING\x1b[0m/g;s/COMPLETED/\x1b[1;32mCOMPLETED\x1b[0m/g;'
 
-squeue -o "${squeue_format}" -S j -u "${USER}" | sed "${sed_colors}"
+squeue -o "${squeue_format}" -u "${USER}" | sort -k5,1 | sed "${sed_colors}"
