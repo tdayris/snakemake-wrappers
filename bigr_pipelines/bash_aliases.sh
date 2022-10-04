b9eca6ac198c089cb3d08d7066709436adb33e35
tdayris <tdayris@gustaveroussy.fr>
2022-10-04 13:50:13 +0200

    new aliases

--- bigr_pipelines/bash_aliases.sh
+++ bigr_pipelines/bash_aliases.sh
@@ -9,6 +9,13 @@ function bigr_conda_activate () {
   conda activate "/mnt/beegfs/pipelines/snakemake-wrappers/bigr_pipelines/common/env2/" ;
 }
 
+# Search a bigr project
+function bigr_search_project () {
+    ls -dlh "/mnt/beegfs/scratch/bioinfo_core/*${1}*" 
+    ls -dlh "/mnt/isilon/data_bioinfo/Projets/*${1}*" 
+    ls -dlh "/mnt/isilon/data_bioinfo/Projets/bioinfo_core/*${1}*"
+}
+
 # Source this file to add new pipelines to your
 # environment. No more loooong command lines,
 # only the best.
@@ -39,5 +46,17 @@ alias bigr_my_jobs="bash /mnt/beegfs/pipelines/snakemake-wrappers/bigr_pipelines
 alias bigr_all_jobs="bash /mnt/beegfs/pipelines/snakemake-wrappers/bigr_pipelines/common/bash/all_jobs.sh"
 alias bigr_post_pipeline="bash /mnt/beegfs/pipelines/snakemake-wrappers/bigr_pipelines/common/python/woops.py"
 
+alias bigr_watch_my_jobs="watch --colors -n 10 bash /mnt/beegfs/pipelines/snakemake-wrappers/bigr_pipelines/common/bash/my_jobs.sh"
+alias bigr_cancel_my_jobs="echo scancel $(squeue -u ${USER} | column -t |cut -f1 -d' ')"
+
 # Multi process a bash command
 alias bigr_multiprocess_bash="bash /mnt/beegfs/pipelines/snakemake-wrappers/bigr_pipelines/common/bash/multiprocess_bash_command.sh"
+
+# Goto functions
+alias bigr_scratch_bioinfo="cd /mnt/beegfs/scratch/bioinfo_core/"
+alias bigr_database="cd /mnt/beegfs/database/bioinfo/Index_DB/"
+alias bigr_userdata="cd /mnt/beegfs/userdata/${USER}/"
+
+alias bigr_meteo="curl https://fr.wttr.in/Villejuif"
+alias bigr_best_employee="echo This user is the bast, by far: ${USER}"
+alias bigr_list_users="who | cut -f 1 -d ' ' | sort | uniq -c"
