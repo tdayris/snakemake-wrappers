15e2ba4d845504c2a1f7ba0f2378b1cfb6c45f7d
tdayris <tdayris@gustaveroussy.fr>
2022-10-18 13:33:58 +0200

    pirest launcher updates

--- bigr_pipelines/fastqc_multiqc/pirest_run.sh
+++ bigr_pipelines/fastqc_multiqc/pirest_run.sh
@@ -3,16 +3,22 @@
 script_dir=$(dirname $0)
 script_dir=$(readlink -e "${script_dir}")
 
-params_demux=""
+if [[ ! "${PATH}" =~ conda ]]
+then
+    # If there is no available conda, use mine
+    source "/mnt/beegfs/userdata/t_dayris/anaconda3/etc/profile.d/conda.sh"
+fi
+
+params_demux="-p demux"
 
 if [ -n "$(find input/*/archive/*/unaligned/Stats/ -name "Stats.json.zip" | head -1)" ]; then
     # Special case demux
-    bash "${script_dir}/run.sh" fastqc_multiqc -p demux output/multiqc.html "$@"
+    bash "${script_dir}/run.sh" fastqc_multiqc output/multiqc.html ${params_demux} "$@"
 else
     echo "Stats not found"
     # Classic case, must fit demux-like behaviour
-    bash "${script_dir}/run.sh" fastqc_multiqc -p demux "$@"
+    bash "${script_dir}/run.sh" fastqc_multiqc ${params_demux} "$@"
     if [ -d "multiqc" ] ; then 
-        mv --verbose multiqc output
+        mv --verbose --force --update multiqc/* output
     fi
 fi
--- bigr_pipelines/fastqc_multiqc/run_demux.sh
+++ bigr_pipelines/fastqc_multiqc/run_demux.sh
@@ -9,16 +9,16 @@ then
     source "/mnt/beegfs/userdata/t_dayris/anaconda3/etc/profile.d/conda.sh"
 fi
 
-params_demux=""
+params_demux="-p demux"
 
 if [ -n "$(find input/*/archive/*/unaligned/Stats/ -name "Stats.json.zip" | head -1)" ]; then
     # Special case demux
-    bash "${script_dir}/run.sh" fastqc_multiqc -p demux output/multiqc.html "$@"
+    bash "${script_dir}/run.sh" fastqc_multiqc ${params_demux} output/multiqc.html "$@"
 else
     echo "Stats not found"
     # Classic case, must fit demux-like behaviour
-    bash "${script_dir}/run.sh" fastqc_multiqc -p demux "$@"
+    bash "${script_dir}/run.sh" fastqc_multiqc ${params_demux} "$@"
     if [ -d "multiqc" ] ; then 
-        mv --verbose multiqc/* output
+        mv --verbose --force --update multiqc/* output
     fi
 fi
