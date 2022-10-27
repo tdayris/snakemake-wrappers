9623364c9e5c0b30facf765c1c13ec3966e51506
tdayris <tdayris@gustaveroussy.fr>
2022-10-27 15:29:11 +0200

    simplify demux

--- bigr_pipelines/fastqc_multiqc/run_demux.sh
+++ bigr_pipelines/fastqc_multiqc/run_demux.sh
@@ -9,23 +9,4 @@ then
     source "/mnt/beegfs/userdata/t_dayris/anaconda3/etc/profile.d/conda.sh"
 fi
 
-params_demux="-p demux"
-
-if [ -f "input/*/archive/uploadToKDIAnalysis/" ]; then
-    echo "uploadToKDIAnalysis found"
-    # Special case InterOp Demux
-    bash "${script_dir}/run.sh" ${params_demux} "$@" multiqc_xml/multiqc.html
-    if [ -d "multiqc" ] ; then 
-        mv --verbose --force --update multiqc_xml/* output
-    fi
-elif [ -n "$(find input/*/archive/*/unaligned/Stats/ -name "Stats.json.zip" | head -1)" ]; then
-    # Base case demux
-    bash "${script_dir}/run.sh" ${params_demux} output/multiqc.html "$@"
-else
-    echo "Stats not found"
-    # Classic case, must fit demux-like behaviour
-    bash "${script_dir}/run.sh" ${params_demux} "$@"
-    if [ -d "multiqc" ] ; then 
-        mv --verbose --force --update multiqc/* output
-    fi
-fi
+bash "${script_dir}/run.sh" -p demux output/multiqc.html "$@"
