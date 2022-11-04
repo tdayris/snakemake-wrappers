91270b3d5bfac3a1b7ab31661afb1859075d3421
tdayris <thibault.dayris@gustaveroussy.fr>
2022-11-04 11:27:58 +0100

    launcher update and Snakemake update

--- bigr_pipelines/snpeff_snpsift/run.sh
+++ bigr_pipelines/snpeff_snpsift/run.sh
@@ -1,63 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# This adds several functions and variable in the environment
-PIPELINE_PREFIX=$(readlink -e "$(dirname ${0})/../..")
-# shellcheck source=/mnt/beegfs/pipelines/snakemake-wrappers/bigr_pipelines/common/bash/messages.sh
-source "${PIPELINE_PREFIX}/bigr_pipelines/common/bash/messages.sh"
-# shellcheck source=/mnt/beegfs/pipelines/snakemake-wrappers/bigr_pipelines/common/bash/environment.sh
-source "${PIPELINE_PREFIX}/bigr_pipelines/common/bash/environment.sh"
+PIPELINE_NAME=$(basename $(dirname "${0}"))
 
-# Define pipeline related variables
-declare -x SNAKEMAKE_PROFILE_PATH="${PIPELINE_PREFIX}/bigr_pipelines/common/profiles"
-declare -x PIPELINE_PATH="${PIPELINE_PREFIX}/bigr_pipelines/snpeff_snpsift"
-export SNAKEMAKE_PROFILE_PATH PIPELINE_PATH
-
-SNAKEFILE_PATH="${PIPELINE_PATH}/Snakefile"
-CONFIG_PATH="${PIPELINE_PATH}/config.hg38.nochr.yaml"
-WRAPPERS_PATH=$(readlink -e "${PIPELINE_PATH}/../../")
-SNAKE_ARGS=("--wrapper-prefix" "${WRAPPERS_PATH}/")
-PROFILE="slurm"
-SUMMARY=""
-GRAPH=""
-
-
-
-while [ "$#" -gt 0 ]; do
-  case "${1}" in
-    -p|--profile) PROFILE="${2}"; shift 2;;
-    --summary) SUMMARY="${2}"; shift 2;;
-    --rulegraph) GRAPH="${2}"; shift 2;;
-    hg19chr|HG19chr) CONFIG_PATH="${PIPELINE_PATH}/config.hg19.yaml"; shift;;
-    hg38chr|HG38chr) CONFIG_PATH="${PIPELINE_PATH}/config.hg38.yaml"; shift;;
-    hg19|HG19) CONFIG_PATH="${PIPELINE_PATH}/config.hg19.nochr.yaml"; shift;;
-    hg38|HG38) CONFIG_PATH="${PIPELINE_PATH}/config.hg38.nochr.yaml"; shift;;
-    mm10|MM10) CONFIG_PATH="${PIPELINE_PATH}/config.mm10.yaml"; shift;;
-    *) SNAKE_ARGS+=("${1}"); shift;;
-  esac
-done
-message INFO "Environment loaded"
-
-if [ ! -f "config.yaml" ]; then
-  rsync -cv "${CONFIG_PATH}" "config.yaml"
-else
-  message INFO "Config file already provided"
-fi
-
-# Run pipeline
-message CMD "conda_activate ${CONDA_ENV_PATH}"
-conda_activate "${CONDA_ENV_PATH}" && message INFO "Conda loaded" || error_handling "${LINENO}" 1 "Could not activate conda environment"
-
-BASE_CMD="snakemake -s ${SNAKEFILE_PATH} --profile ${SNAKEMAKE_PROFILE_PATH}/${PROFILE} ${SNAKE_ARGS[*]}"
-if [ "${SUMMARY}" != "" ]; then
-  SUMMARY_CMD="${BASE_CMD} --summary > ${SUMMARY}"
-  message CMD "${SUMMARY_CMD}"
-  eval SUMMARY_CMD
-elif [ "${GRAPH}" != "" ]; then
-  RULEGRAPH_CMD="${BASE_CMD} --rulegraph | dot -Tpng > ${GRAPH}"
-  message CMD "${RULEGRAPH_CMD}"
-  eval ${SUMMARY_CMD}
-else
-  message CMD "${BASE_CMD}"
-  eval ${BASE_CMD}
-fi
+bash "$(dirname ${0})/../common/bash/run.sh" --name ${PIPELINE_NAME} "$@"
