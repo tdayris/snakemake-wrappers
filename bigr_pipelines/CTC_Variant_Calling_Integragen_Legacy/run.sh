5e76d12d06c49ad65e23a4068d2d3b4a789e79b8
tdayris <thibault.dayris@gustaveroussy.fr>
2023-01-04 10:37:01 +0100

    use singularity by default

--- bigr_pipelines/CTC_Variant_Calling_Integragen_Legacy/run.sh
+++ bigr_pipelines/CTC_Variant_Calling_Integragen_Legacy/run.sh
@@ -3,4 +3,4 @@ set -e
 
 PIPELINE_NAME=$(basename $(dirname "${0}"))
 
-bash "$(dirname ${0})/../common/bash/run.sh" --name ${PIPELINE_NAME} "$@"
+bash "$(dirname ${0})/../common/bash/run.sh" --name ${PIPELINE_NAME} --use-singularity "$@"
