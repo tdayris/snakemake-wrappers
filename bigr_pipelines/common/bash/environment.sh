5eb12916aa732128ebc2ca1ced98cd8c0d23e6dd
tdayris <thibault.dayris@gustaveroussy.fr>
2023-01-04 09:49:51 +0100

    new tmp overloading

--- bigr_pipelines/common/bash/environment.sh
+++ bigr_pipelines/common/bash/environment.sh
@@ -80,4 +80,123 @@ CMD="mkdir --parents --verbose tmp/shadow"
 message CMD "${CMD}"
 eval ${CMD}
 
+# Overloading TMP directories
+# Our main temporary directory.
+BIGR_DEFAULT_TMP="/mnt/beegfs/userdata/${USER}/tmp"
+
+if [ ! -d "${BIGR_DEFAULT_TMP}" ]; then
+  CMD="mkdir --parent --verbose ${BIGR_DEFAULT_TMP}"
+  message CMD "${CMD}"
+  eval ${CMD}
+fi
+
+# Used in many bash / Python scripts
+if [ -z ${TMP} ]; then
+  message WARNING "TMP environment variable was not set. This can lead to OS errors due to lack of space in /tmp"
+  message WARNING "TMP now points to ${BIGR_DEFAULT_TMP}"
+  declare -x TMP
+  TMP="${BIGR_DEFAULT_TMP}"
+  export TMP
+elif [ "${TMP}" == "/tmp" ]; then
+  message WARNING "TMP currently points to '/tmp'. This can lead to OS errors due to lack of space in /tmp"
+  message WARNING "This value is now changed to ${BIGR_DEFAULT_TMP}"
+  TMP="${BIGR_DEFAULT_TMP}"
+  export TMP
+else
+  message INFO "TMP -> ${TMP}"
+fi
+
+# Used in some bash / R / perl / Python scripts
+if [ -z ${TEMP} ]; then
+  message WARNING "TEMP environment variable was not set. This can lead to OS errors due to lack of space in /tmp"
+  message WARNING "TEMP now points to ${BIGR_DEFAULT_TMP}"
+  declare -x TEMP
+  TEMP="${BIGR_DEFAULT_TMP}"
+  export TEMP
+elif [ "${TEMP}" == "/tmp" ]; then
+  message WARNING "TEMP currently points to '/tmp'. This can lead to OS errors due to lack of space in /tmp"
+  message WARNING "TEMP now points to ${BIGR_DEFAULT_TMP}"
+  TEMP="${BIGR_DEFAULT_TMP}"
+  export TEMP
+else
+  message INFO "TEMP -> ${TEMP}"
+fi
+
+# Used in some bash / R / perl / Python scripts
+if [ -z ${TMPDIR} ]; then
+  message WARNING "TMPDIR environment variable was not set. This can lead to OS errors due to lack of space in /tmp"
+  message WARNING "TMPDIR now points to ${BIGR_DEFAULT_TMP}"
+  declare -x TMPDIR
+  TMPDIR="${BIGR_DEFAULT_TMP}"
+  export TMPDIR
+elif [ "${TMPDIR}" == "/tmp" ]; then
+  message WARNING "TMPDIR currently points to '/tmp'. This can lead to OS errors due to lack of space in /tmp"
+  message WARNING "TMPDIR now points to ${BIGR_DEFAULT_TMP}"
+  TMPDIR="${BIGR_DEFAULT_TMP}"
+  export TMPDIR
+else
+  message INFO "TMPDIR -> ${TMPDIR}"
+fi
+
+# Used in some bash / R / perl scripts
+if [ -z ${TEMPDIR} ]; then
+  message WARNING "TEMPDIR environment variable was not set. This can lead to OS errors due to lack of space in /tmp"
+  message WARNING "TEMPDIR now points to ${BIGR_DEFAULT_TMP}"
+  declare -x TEMPDIR
+  TEMPDIR="${BIGR_DEFAULT_TMP}"
+  export TEMPDIR
+elif [ "${TEMPDIR}" == "/tmp" ]; then
+  message WARNING "TEMPDIR currently points to '/tmp'. This can lead to OS errors due to lack of space in /tmp"
+  message WARNING "TEMPDIR now points to ${BIGR_DEFAULT_TMP}"
+  TEMPDIR="${BIGR_DEFAULT_TMP}"
+  export TEMPDIR
+else
+  message INFO "TEMPDIR -> ${TEMPDIR}"
+fi
+
+# Used in nextflow scripts
+if [ -z ${NXF_TEMP} ]; then
+  message WARNING "NXF_TEMP environment variable was not set. This can lead to NextFlow errors due to lack of space in /tmp"
+  message WARNING "NXF_TEMP now points to ${BIGR_DEFAULT_TMP}"
+  declare -x NXF_TEMP
+  NXF_TEMP="${BIGR_DEFAULT_TMP}"
+  export NXF_TEMP
+elif [ "${NXF_TEMP}" == "/tmp" ]; then
+  message WARNING "NXF_TEMP currently points to '/tmp'. This can lead to OS errors due to lack of space in /tmp"
+  message WARNING "NXF_TEMP now points to ${BIGR_DEFAULT_TMP}"
+  NXF_TEMP="${BIGR_DEFAULT_TMP}"
+  export NXF_TEMP
+else
+  message INFO "NXF_TEMP -> ${NXF_TEMP}"
+fi
+
+# Used in nextflow / java scripts
+if [ -z ${_JAVA_OPTIONS} ]; then
+  message WARNING "_JAVA_OPTIONS environment variable was not set. This can lead to Java errors due to lack of space in /tmp"
+  message WARNING "_JAVA_OPTIONS now points to ${BIGR_DEFAULT_TMP}"
+  declare -x _JAVA_OPTIONS
+  _JAVA_OPTIONS="${BIGR_DEFAULT_TMP}"
+  export _JAVA_OPTIONS
+elif [ "${_JAVA_OPTIONS}" == "/tmp" ]; then
+  message WARNING "_JAVA_OPTIONS currently points to '/tmp'. This can lead to OS errors due to lack of space in /tmp"
+  message WARNING "_JAVA_OPTIONS now points to ${BIGR_DEFAULT_TMP}"
+  _JAVA_OPTIONS="${BIGR_DEFAULT_TMP}"
+  export _JAVA_OPTIONS
+else
+  message INFO "_JAVA_OPTIONS -> ${_JAVA_OPTIONS}"
+fi
+
+if [ ! -f "~/.Renviron" ]; then
+  message WARNING "~/.Renviron does not exists. This can lead to OS errors in R due to lack of space in /tmp"
+  message WARNING "~/.Renviron was created with: TMP = '${BIGR_DEFAULT_TMP}'"
+  echo -e "TMP = '${BIGR_DEFAULT_TMP}'" > "~/.Renviron"
+fi
+
+if [ ! -f "~/.condarc" ]; then
+  message WARNING "~/.condarc does not exists. This can lead to OS errors in conda due to lack of space in /tmp"
+  message WARNING "~/.condarc was created with: env_dir, pkgs_dir, and conda-build:root_dir overloaded"
+  echo -e "envs_dir:\n\t- /mnt/beegfs/userdata/${USER}/anaconda/envs\npkgs_dir:\n\t- /mnt/beegfs/userdata/${USER}/anaconda/pkgs\nconda-build:\n\troot_dir: /mnt/beegfs/userdata/${USER}/conda-builds" > "~/.condarc"
+fi
+
+
 message INFO "sourced: environment.sh"
