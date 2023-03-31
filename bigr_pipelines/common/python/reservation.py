96bcd65a2df9294fcb655ab9784eca6b7aeac166
tdayris <thibault.dayris@gustaveroussy.fr>
2023-03-31 10:06:04 +0200

    new reservation scheme

--- bigr_pipelines/common/python/reservation.py
+++ bigr_pipelines/common/python/reservation.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import dataclasses
 import functools
+import os.path
+
+# Used to mimic snakemake input interface
+@dataclasses.dataclass
+class SmallInput:
+    size_mb: int
 
 
 # Memory and time reservation
@@ -53,7 +60,9 @@ get_5h_and_10h_per_attempt = functools.partial(
 # Explicit memory reservation
 get_768mb_per_attempt = functools.partial(get_resources_per_attempt, multiplier=768)
 get_1gb_per_attempt = functools.partial(get_resources_per_attempt, multiplier=1024)
-get_1p5gb_per_attempt = functools.partial(get_resources_per_attempt, multiplier=1024 * 1.5)
+get_1p5gb_per_attempt = functools.partial(
+    get_resources_per_attempt, multiplier=1024 * 1.5
+)
 get_2gb_per_attempt = functools.partial(get_resources_per_attempt, multiplier=1024 * 2)
 get_3gb_per_attempt = functools.partial(get_resources_per_attempt, multiplier=1024 * 3)
 get_4gb_per_attempt = functools.partial(get_resources_per_attempt, multiplier=1024 * 4)
@@ -61,9 +70,15 @@ get_5gb_per_attempt = functools.partial(get_resources_per_attempt, multiplier=10
 get_6gb_per_attempt = functools.partial(get_resources_per_attempt, multiplier=1024 * 6)
 get_7gb_per_attempt = functools.partial(get_resources_per_attempt, multiplier=1024 * 7)
 get_8gb_per_attempt = functools.partial(get_resources_per_attempt, multiplier=1024 * 8)
-get_10gb_per_attempt = functools.partial(get_resources_per_attempt, multiplier=1024 * 10)
-get_15gb_per_attempt = functools.partial(get_resources_per_attempt, multiplier=1024 * 15)
-get_20gb_per_attempt = functools.partial(get_resources_per_attempt, multiplier=1024 * 20)
+get_10gb_per_attempt = functools.partial(
+    get_resources_per_attempt, multiplier=1024 * 10
+)
+get_15gb_per_attempt = functools.partial(
+    get_resources_per_attempt, multiplier=1024 * 15
+)
+get_20gb_per_attempt = functools.partial(
+    get_resources_per_attempt, multiplier=1024 * 20
+)
 get_2gb_and_6gb_per_attempt = functools.partial(
     get_resources_per_attempt, multiplier=1025 * 6, base=1024 * 2
 )
@@ -101,7 +116,7 @@ def get_resources_per_gb(
         # Case there is 1gb or more in input
         (input.size_mb * multiplier),
         # Case there is less than 1gb in input
-        base
+        base,
     )
 
 
@@ -109,4 +124,82 @@ def get_resources_per_gb(
 get_half_input_size = functools.partial(get_resources_per_gb, multiplier=0.5)
 get_same_input_size = functools.partial(get_resources_per_gb, multiplier=1, base=1024)
 get_twice_input_size = functools.partial(get_resources_per_gb, multiplier=2)
-get_4gb_per_input_size = functools.partial(get_resources_per_gb, multiplier=1024 * 10, base = 1024 * 10)
+get_4gb_per_input_size = functools.partial(
+    get_resources_per_gb, multiplier=1024 * 10, base=1024 * 10
+)
+
+
+# Time reservation based on input size
+def get_resources_per_gb_per_attempt(
+    wildcards, input, attempt, multiplier: int = 1, base: int = 1024
+) -> int:
+    """
+    Return the amount of resources needed per GB of input.
+
+    Parameters:
+    * wildcards: Snakemake signature requires this parameter.
+    * input: The input file list
+    * attempt: The # of times the calling rule has been restarted
+    * multiplier: An arbitrary multiplier
+
+    Return:
+    (int) The amount of resources needed (mb, minutes, etc)
+    """
+    return max(
+        # Case there is more than ~1gb in input
+        int((input.size_mb / 1024) * multiplier * attempt) + base * attempt,
+        # Case there is less than ~1gb in input
+        base * attempt,
+    )
+
+
+# Explicit time reservation based on input size and attempts
+get_15min_per_gb_input_per_attempt = functools.partial(
+    get_resources_per_gb_per_attempt, multiplier=15, base=15
+)
+get_45min_per_gb_input_per_attempt = functools.partial(
+    get_resources_per_gb_per_attempt, multiplier=45, base=45
+)
+
+
+# Explicit memory reservation based on input size and attempts
+get_4gb_per_gb_input_per_attempt = functools.partial(
+    get_resources_per_gb_per_attempt, multiplier=4 * 1024, base=4 * 1024
+)
+
+
+# Some special cases
+def get_salmon_time_per_attempt(wildcards, input, attempt) -> int:
+    """Special case for salmon and its index size"""
+    sequence_size = 0
+    if "r1" in input.keys():
+        sequence_size += os.path.getsize(str(input["r1"]))
+    
+    if "r2" in input.keys():
+        sequence_size += os.path.getsize(str(input["r2"]))
+
+    return get_resources_per_gb_per_attempt(
+        wildcards=wildcards,
+        input=SmallInput(size_mb=sequence_size),
+        attempt=attempt,
+        multiplier=60,
+        base=90,
+    )
+
+
+def get_salmon_memory_per_attempt(wildcards, input, attempt) -> int:
+    """Special case for salmon and its index size"""
+    sequence_size = 0
+    if "r1" in input.keys():
+        sequence_size += os.path.getsize(str(input["r1"]))
+    
+    if "r2" in input.keys():
+        sequence_size += os.path.getsize(str(input["r2"]))
+
+    return get_resources_per_gb_per_attempt(
+        wildcards=wildcards,
+        input=SmallInput(size_mb=sequence_size),
+        attempt=attempt,
+        multiplier=10 * 1024,
+        base=15 * 1024,
+    )
