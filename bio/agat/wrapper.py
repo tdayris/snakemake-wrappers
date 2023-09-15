104ea29d7c685a0080b068e18f265d9d4acca4ff
tdayris <thibault.dayris@gustaveroussy.fr>
2023-09-15 14:02:30 +0200

    dev

--- bio/agat/wrapper.py
+++ bio/agat/wrapper.py
@@ -5,7 +5,7 @@ __copyright__ = "Copyright 2023, Thibault Dayris"
 __email__ = "thibault.dayris@gustaveroussy.fr"
 __license__ = "MIT"
 
-import os.path
+import shlex
 
 from snakemake.shell import shell
 from tempfile import TemporaryDirectory
@@ -35,31 +35,20 @@ for argname, file_path in snakemake.input.items():
     else:
         input_cmd += f" {dash}{argname} {file_path} "
 
-# Acquire output file(s)
-# output_cmd = ""
-# if script == "agat_convert_sp_gff2zff.pl":
-#     output_cmd += f""
-#     prefix = os.path.commonprefix(list(map(str, snakemake.output)))[:-1]
-#     output_cmd += f" --output {prefix} "
-# elif script == "agat_sp_extract_attributes.pl":
-#     ext = os.path.splitext(snakemake.output[0])[-1]
-#     prefix = os.path.commonprefix(list(map(str , snakemake.output)))[:-1]
-#     output_cmd += f" --output {prefix}{ext} "
-
-# else:
-
-
 
 with TemporaryDirectory() as tempdir:
     output_cmd = ""
     # The following script do not let user choose output file names
-    if script in ["agat_convert_sp_gff2zff.pl", "agat_sp_extract_attributes.pl"]:
+    if script in ["agat_sp_extract_attributes.pl"]:
         output_cmd += f" --output {tempdir}/outfile "
+    elif script in ["agat_sp_filter_by_ORF_size.pl"]:
+        output_cmd += f" --output {tempdir}/outfile.gff "
     else:
         for argname, file_path in snakemake.output.items():
             # Deal with long/short options, since some agat scripts
             # only accepts 'output' as long option and output.output is
             # a protected name in Snakemake.
+            argname = "output" if script in ["agat_convert_sp_gff2zff.pl"] else argname
             dash = "-" if len(str(argname)) == 1 else "--"
             output_cmd += f" {dash}{argname} {file_path} "
 
@@ -70,24 +59,18 @@ with TemporaryDirectory() as tempdir:
 
     # Forwarding output files for script that do not
     # let user choose output file name(s)
-    if script == "agat_convert_sp_gff2zff.pl":
-        annot = snakemake.output.get("annotation")
-        if annot:
-            shell(f"mv --verbose {tempdir}/outfile.ann {annot} {log}")
-        
-        fasta = snakemake.output.get("fasta")
-        if fasta:
-            shell(f"mv --verbose {tempdir}/outfile.dna {fasta} {log}")
-
-    elif script == "agat_sp_extract_attributes.pl":
-        extra_args = iter(extra.split(" "))
+    if script == "agat_sp_extract_attributes.pl":
+        extra_args = iter(shlex.split(extra))
         extra = next(extra_args, None)   
         
         # Acquire all expected output files
         fields = []
         while extra is not None:
             if extra in ["--attribute", "--att", "-a"]:
-                fields = next(extra_args, None).split(",")
+                if "=" in extra:
+                    fields = extra.split("=")[-1]
+                else:
+                    fields = next(extra_args, None).split(",")
                 break
 
             next(extra_args, None)
@@ -96,5 +79,32 @@ with TemporaryDirectory() as tempdir:
         # order as the attributes listed in `extra`
         for field, outfile in zip(fields, snakemake.output):
             shell(f"mv --verbose {tempdir}/outfile_{field} {outfile} {log}")
+
+    elif script == "agat_sp_filter_by_ORF_size.pl":
+        filters = ["sup", "NOT_sup"]
+        prot_length = 100
+
+        # Acquiering protein size chosen by user (if any)
+        extra_args = iter(shlex.split(extra))
+        extra = next(extra_args, None)
+
+        while extra is not None:
+            if extra in ["--size", "-s"]:
+                if "=" in extra:
+                    prot_length = extra.split("=")[-1]
+                else:
+                    prot_length = next(extra_args, None).split(",")
+                break
+
+            next(extra_args, None)
+        
+        # Move expected output files as uses intends
+        passing = snakemake.output.get("passing")
+        if passing:
+            shell("mv --verbose {tempdir}/outfile_{fiters[0]}{prot_length}.gff {passing} {log}")
+        
+        not_passing = snakemake.output.get("not_passing")
+        if not_passing:
+            shell("mv --verbose {tempdir}/outfile.{fiters[1]}{prot_length}.gff {not_passing} {log}")
             
         
