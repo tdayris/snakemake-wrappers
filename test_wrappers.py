7a8fa1fd6a3981921a58e799464a86b7e0634889
tdayris <thibault.dayris@gustaveroussy.fr>
2026-08-13 14:40:22 +0200

    feat: add xan test

--- test_wrappers.py
+++ test_wrappers.py
@@ -1240,6 +1240,21 @@ def test_csvtk(run):
     )
 
 
+def test_xan(run):
+    run(
+        "utils/xan/run",
+        [
+            "snakemake", 
+            "processed_through_script.csv", 
+            "processed_through_expression.csv",
+        ],
+        compare_results_with_expected={
+            "processed_through_script.csv": "expected/total_reads.csv",
+            "processed_through_expression.csv": "expected/total_fragments.csv",
+        },
+    )
+
+
 def test_xsv(run):
     run(
         "utils/xsv",
